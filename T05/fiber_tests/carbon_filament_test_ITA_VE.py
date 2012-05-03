208d646cdf8c07943d4722cf062c06e780badedf
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2012-05-03 14:05:55 +0200

    filament carbon tests added

---
+++ T05/fiber_tests/carbon_filament_test_ITA_VE.py
@@ -0,0 +1,44 @@
+'''
+Created on May 3, 2012
+
+@author: rch
+'''
+
+
+import math
+
+# cross section
+d = 7.8e-3
+r = d / 2.0
+
+A = math.pi * r ** 2
+
+# length 2.5 mm
+
+F_25 = 0.17
+F_25_std = 0.0238
+
+# reduction factor for a bundle
+bundle_factor = 0.8
+
+print 'A', A
+print 'F_25', F_25
+print 'F_cov_25', F_25_std / F_25
+sig_25 = F_25 / A
+
+print 'sig_25_filament', sig_25 
+print 'sig_25_bundle', sig_25 * bundle_factor
+
+# length 50 mm
+
+F_50 = 0.155
+F_50_std = 0.0195
+
+print 'F_50', F_50
+print 'F_cov_50', F_50_std / F_50
+sig_50 = F_50 / A
+print 'sig_50_filament', sig_50 
+
+print 'sig_50_bundle', sig_50 * bundle_factor
+
+
