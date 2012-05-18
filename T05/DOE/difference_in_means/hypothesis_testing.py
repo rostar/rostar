f2c441dfa11da84ab8516d618255f96d29bd2a4b
rostar <rrypl@.imb.rwth-aachen.de>
2012-05-18 11:01:17 +0200

    two sample T test implemented for 2 alternative hypotheses: mean1 > mean2; mean1 < mean2; H0: mean1 = mean2

--- T05/DOE/difference_in_means/hypothesis_testing.py
+++ T05/DOE/difference_in_means/hypothesis_testing.py
@@ -17,7 +17,7 @@ class TTest(HasTraits):
     
     data = Instance(DiffMeansDataInput, ())
 
-    alpha = Float(0.5)
+    alpha = Float(0.05)
     ''' level of significance (type I error) = P(H0 rejected|H0 true) '''
 
     t0 = Property(depends_on = 'data.data1, data.data2')
@@ -37,18 +37,18 @@ class TTest(HasTraits):
         n1 = len(self.data.data1)
         n2 = len(self.data.data2)
         print 'at the level of significance ', self.alpha, ':'
-        res0 = t.ppf(1 - self.alpha/2., n1 + n2 -2) > np.abs(self.t0)
-        print 'mu1 = mu2 is ',res0
-        res1 = t.ppf(1 - self.alpha/2., n1 + n2 -2) < np.abs(self.t0)
-        print 'mu1 != mu2 is ',res1
-        res2 = t.ppf(1-self.alpha, n1 + n2 -2) < self.t0
-        print 'mu1 > mu2 is ',res2
-        res3 = -t.ppf(1-self.alpha, n1 + n2 -2) > self.t0
-        print 'mu1 < mu2 is ',res3        
+        #H0 = t.ppf(1 - self.alpha/2., n1 + n2 -2) > np.abs(self.t0)
+        H1a = t.ppf(1-self.alpha, n1 + n2 -2) < self.t0
+        H1b = t.ppf(self.alpha, n1 + n2 -2) > self.t0
+        print 'mu1 = mu2 is ',H1a == H1b
+        print 'mu1 > mu2 is ',H1a
+        print 'mu1 < mu2 is ',H1b        
     
 if __name__ == '__main__':
-    tt = TTest()
-    tt.data.data1 = norm.rvs( loc = 20, size = 10)
-    tt.data.data2 = norm.rvs(loc = 20, size = 10)
+    from matplotlib import pyplot as plt
+    tt = TTest(alpha = 0.0001)
+    tt.data.data1 = np.array([460., 415,486,488,493,443,512,512,569,487,
+                              458,426,454,387,464,524,551,521,462,579])
+    tt.data.data2 = np.array([542.,503,578,533,572,537,543,538,582,563,
+                              572,541,541,561,526,535,474,536,568,503])
     tt.evaluate()
-    
