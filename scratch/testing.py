930c43093ad6c7918d126d09605b35aa3c474735
rostar <rrypl@.imb.rwth-aachen.de>
2012-09-05 17:09:20 +0200

    scratch backup

--- scratch/testing.py
+++ scratch/testing.py
@@ -5,19 +5,12 @@ Created on 14.6.2012
 @author: Q
 '''
 import numpy as np
-from scipy.optimize import brentq, fsolve
-from scipy.stats import weibull_min
+from scipy.optimize import fsolve
+from scipy.stats import weibull_min, uniform
 from matplotlib import pyplot as plt
-from stats.spirrid.spirrid import SPIRRID
-from stats.spirrid.rv import RV
-from quaducom.micro.resp_func.cb_emtrx_clamped_fiber_stress import \
-    CBEMClampedFiberStress
-from quaducom.micro.resp_func.cb_emtrx_clamped_fiber_stress_residual import \
-    CBEMClampedFiberStressResidual
-from math import e
 from etsproxy.traits.api import HasTraits, cached_property, Float, Property
 from mathkit.mfn.mfn_line.mfn_line import MFnLineArray
-
+import copy
 
 class Profile(HasTraits):
 
@@ -48,36 +41,80 @@ class Profile(HasTraits):
         return self.Km + self.Kf
 
     def tau_distr(self):
-        return weibull_min(self.mtau, scale=self.stau)
+        return uniform(loc=.2, scale=2.)
+        #return weibull_min(self.mtau, scale=self.stau)
 
-    Tmfn = Property(depends_on='V_f, mtau, stau, r, E_m, E_f, w')
+    def T_epsm(self, tau):
+        return 2. * tau * self.V_f / self.r / self.E_m / (1. - self.V_f)
 
-    @cached_property
-    def _get_Tmfn(self):
-        tau_arr = np.linspace(0.0, self.tau_distr().ppf(0.99), 500)
-        a = lambda t: np.sqrt(2. * self.w * self.Km /
-                      (2./self.r/self.E_f *
-                       (t + np.trapz(self.tau_distr().cdf(tau_arr), tau_arr))))
-        a_vect = np.vectorize(a)
-        x_arr = a_vect(tau_arr)
-        return MFnLineArray(xdata=x_arr[::-1], ydata=2./self.r/self.E_f*tau_arr[::-1])
+    def T_epsf(self, tau):
+        return 2. * tau / self.r / self.E_f
 
-    def T_x(self, x):
-        return self.Tmfn.get_values(x)
+    def deps_m(self, T):
+        tau_max = T * self.r * (1 - self.V_f) * self.E_m / 2. / self.V_f
+        tau_min = self.tau_distr().ppf(0.0001)
+        range = self.tau_distr().ppf(0.9999) - self.tau_distr().ppf(0.0001)
+        if tau_max > self.tau_distr().ppf(0.9999):
+            tau_max = self.tau_distr().ppf(0.9999) + 0.01 * range
+        tau_arr = np.linspace(tau_min - 0.01 * range, tau_max, 500)
+        return self.T_epsm(np.trapz(tau_arr * self.tau_distr().pdf(tau_arr), tau_arr))
 
+    def residuum(self, Tepsm, eps_m_x, x_x):
+        tau = Tepsm * self.r * (1 - self.V_f) * self.E_m / 2. / self.V_f
+        res = eps_m_x[-1] * x_x[-1] + self.T_epsf(tau) * x_x[-1] ** 2 / 2. - np.trapz(eps_m_x, x_x)
+        return self.w / 2. - res
 
+    def eps_m(self, x_arr):
+        eps_m_x = [0.0]
+        deps_m_x = [self.T_epsm(self.tau_distr().mean())]
+        T_epsm_x = []
+        x_x = [0.0]
+        for xi in x_arr[1:]:
+            fwd_Euler = False
+            midpoint_method = True
+            if fwd_Euler == True:
+                eps_m_x.append(eps_m_x[-1] + deps_m_x[-1] * (xi - x_x[-1]))
+            elif midpoint_method == True:
+                h = (xi - x_x[-1])
+                eps_midp = (eps_m_x[-1] + h/2. * deps_m_x[-1])
+                eps_lst = copy.deepcopy(eps_m_x)
+                eps_lst.append(eps_midp)
+                x_lst = copy.deepcopy(x_x)
+                x_lst.append(x_x[-1]+h/2.)
+                T_midp = fsolve(self.residuum, 0.0, args=(eps_lst, x_lst))
+                deps_midp = self.deps_m(T_midp)
+                eps_m_x.append(eps_m_x[-1] + h * deps_midp)
+            x_x.append(xi)
+            T_epsm_x.append(fsolve(self.residuum, 0.0, args=(eps_m_x, x_x)))
+            deps_m_x.append(self.deps_m(T_epsm_x[-1]))
+        t_arr = np.array([2.0] + T_epsm_x).flatten()
+        x_arr = np.array(x_x)
+        epsm = np.array(eps_m_x)
+        tau_x = MFnLineArray(ydata=x_arr[::-1], xdata=(t_arr * self.r *
+                                                       (1 - self.V_f) *
+                                                       self.E_m / 2. / self.V_f)[::-1])
+        x_epsm = MFnLineArray(xdata=x_arr, ydata=epsm)
+        taufil_arr = np.linspace(0.225, 2.175, 40)
+        xfil_arr = tau_x.get_values(taufil_arr)[::-1]
+        x_arr = np.hstack((np.linspace(0.0, xfil_arr[0] - 0.1, 50), xfil_arr))
+        for i, t in enumerate(taufil_arr[::-1]):
+            t = self.T_epsf(t)
+            x = xfil_arr[:i + 1]
+            epsm = x_epsm.get_values(x_arr)
+            epsf = epsm[i] + x[-1] * t - x_arr * t
+            epsf = np.maximum(epsf, epsm)
+            print np.trapz(epsf - epsm, x_arr)
+            plt.plot(x_arr, epsf)
+            plt.plot(x_arr, epsm)
+        plt.show()
+        return eps_m_x
 
 if __name__ == '__main__':
     p = Profile()
-    x = np.linspace(0, 6000, 100)
-    t = p.T_x(x)
-    plt.plot(x, t)
+    x = np.linspace(0, 30., 100)
+    plt.plot(x, p.eps_m(x))
+    #plt.show()
+    #t = np.linspace(0.,20.,100)
+    #plt.plot(p.residuum(t, [0.0], [0.0]))
     plt.show()
-    
-
-
-
-
-
-
 
