7c842f6e61853f6a99048d48141ab63750c9927a
rostar <rrypl@.imb.rwth-aachen.de>
2012-09-13 15:26:38 +0200

    optimizing procedure

--- scratch/testing.py
+++ scratch/testing.py
@@ -92,32 +92,33 @@ if __name__ == '__main__':
         r = 0.00345
         l = 1.0
         theta = 0.0
-        xi = 2.0
+        xi = .013
         phi = 1.
-        E_m = 28e3
-        E_f = 180e3
-        length = 550.
+        E_m = 25e3
+        E_f = 200e3
+        length = 1500.
         nx = 1500
-        smallest_resid = 30.
-        t_shape = 1.0
+        smallest_resid = 300.
+        t_shape = 1.
+        mtrx_scale = 5.8
+        mtrx_shape = 21.0
 
-        def get_eps_extrap(self, t_scale, mtrx_shape, mtrx_scale):
+        def get_eps_extrap(self, t_scale, ef):
             tau = RV('weibull_min', shape=self.t_shape, scale=t_scale)
             random_field = RandomField(seed=True,
                                        lacor=5.,
                                         xgrid=np.linspace(0., self.length, 500),
                                         nsim=1,
                                         loc=.0,
-                                        shape=mtrx_shape,
-                                        scale=mtrx_scale,
-                                        non_negative_check=True,
+                                        shape=self.mtrx_shape,
+                                        scale=self.mtrx_scale * 1.1,
                                         distribution='Weibull'
                                        )
             rf = CBEMClampedFiberStressSP()
             rand = FunctionRandomization(q=rf,
                                          tvars=dict(tau=tau,
                                                     l=self.l,
-                                                    E_f=self.E_f,
+                                                    E_f=ef,
                                                     theta=self.theta,
                                                     xi=self.xi,
                                                     phi=self.phi,
@@ -145,15 +146,15 @@ if __name__ == '__main__':
             scm_view.model.evaluate()
             return scm_view.eps_sigma
 
-        def get_eps(self, t_scale, mtrx_shape, mtrx_scale):
+        def get_eps(self, t_scale, ef):
             tau = RV('weibull_min', shape=self.t_shape, scale=t_scale)
             random_field = RandomField(seed=True,
                                        lacor=5.,
                                         xgrid=np.linspace(0., self.length, 500),
                                         nsim=1,
                                         loc=.0,
-                                        shape=mtrx_shape,
-                                        scale=mtrx_scale,
+                                        shape=self.mtrx_shape,
+                                        scale=self.mtrx_scale,
                                         non_negative_check=True,
                                         distribution='Weibull'
                                        )
@@ -161,7 +162,7 @@ if __name__ == '__main__':
             rand = FunctionRandomization(q=rf,
                                          tvars=dict(tau=tau,
                                                     l=self.l,
-                                                    E_f=self.E_f,
+                                                    E_f=ef,
                                                     theta=self.theta,
                                                     xi=self.xi,
                                                     phi=self.phi,
@@ -178,7 +179,7 @@ if __name__ == '__main__':
                       cb_randomization=rand,
                       cb_type='mean',
                       load_sigma_c_min=.1,
-                      load_sigma_c_max=10.,
+                      load_sigma_c_max=12.,
                       load_n_sigma_c=100,
                       n_w=50,
                       n_x=61,
@@ -198,20 +199,18 @@ if __name__ == '__main__':
 
         def residuum(self, params):
             t_scale = params[0]
-            mtrx_shape = params[1]
-            mtrx_scale = params[2]
+            ef = params[1]
             try:
-                eps_opt, sigma = self.get_eps(t_scale, mtrx_shape, mtrx_scale)
+                eps_opt, sigma = self.get_eps(t_scale, ef)
                 squares = (self.experiment(eps_opt) - sigma) ** 2
                 weight = np.ones(len(squares))
-                weight[50:] = 3.
+                weight[50:70] = 5.
                 resid = np.sum((self.experiment(eps_opt) - sigma) ** 2 * weight)
             except:
                 print '!!!!FAILED!!!!'
                 resid = self.smallest_resid * 20.0
             print 't_scale', t_scale
-            print 'mtrx_shape', mtrx_shape
-            print 'mtrx_scale', mtrx_scale
+            print 'Ef', ef
             print 'RESIDUUM', resid
             if resid < self.smallest_resid:
                 self.smallest_resid = resid
@@ -219,41 +218,34 @@ if __name__ == '__main__':
                 plt.plot(V13.ex_type.eps_asc, V13.ex_type.sig_c_asc, color='black')
                 plt.plot(V23.ex_type.eps_asc, V23.ex_type.sig_c_asc, color='black')
                 plt.plot(V33.ex_type.eps_asc, V33.ex_type.sig_c_asc, color='black')
-                plt.plot(eps_opt, sigma, color='red')
-                plt.plot(V1small.ex_type.eps_asc, V1small.ex_type.sig_c_asc, color='black')
+                plt.plot(eps_opt, sigma, color='red',
+                         label='%.2f, %.2f' %(t_scale, ef))
                 plt.plot(V2small.ex_type.eps_asc, V2small.ex_type.sig_c_asc, color='black')
                 plt.plot(V3small.ex_type.eps_asc, V3small.ex_type.sig_c_asc, color='black')
                 try:
                     eps_extrap, sigma_extrap = self.get_eps_extrap(t_scale,
-                                                          mtrx_shape, mtrx_scale)
+                                                                   ef)
                     plt.plot(eps_extrap, sigma_extrap, color='red')
                 except:
                     pass
                 plt.xlim(0., 0.008)
                 plt.ion()
-                plt.draw()
+                plt.legend(loc='best')
             plt.show()
-            return np.array([resid] * 3)
+            return resid
 
         def get_parameters(self):
-            opt_params = fsolve(self.residuum, np.array([0.1, 17., 7.0]))
+            opt_params = fmin(self.residuum, np.array([0.087, 201e3]))
             print opt_params
-
-    opt = Opt()
-    opt.get_parameters()
 
     def plot():
 
         # access the response values.
-        for V in [V14, V24, V34]:
-            eps = V.ex_type.eps_asc
-            sig_c = V.ex_type.sig_c_asc
-            plt.plot(eps, sig_c, color='red')
         for V in [V13, V23, V33]:
             eps = V.ex_type.eps_asc
             sig_c = V.ex_type.sig_c_asc
             plt.plot(eps, sig_c, color='blue')
-        for V in [V1small, V2small, V3small]:
+        for V in [V2small, V3small]:
             eps = V.ex_type.eps_asc
             sig_c = V.ex_type.sig_c_asc
             plt.plot(eps, sig_c, color='green')
@@ -261,29 +253,29 @@ if __name__ == '__main__':
 
     # filaments
         r = 0.00345
-        tau = RV('weibull_min', shape=1.013, scale=.02066)
-        Ef = 198e3
-        Em = 26e3
+        tau = RV('weibull_min', shape=1.0, scale=.086)
+        Ef = 195e3
+        Em = 25e3
         l = 1.0#RV('uniform', scale=6., loc=.5)
         theta = 0.0
-        xi = 2.0#RV('weibull_min', scale=0.02, shape=5)
+        xi = 20.013#RV('weibull_min', scale=0.02, shape=5)
         phi = 1.
 
-        length = 550.
+        length = 1500.
         nx = 1500
-        random_field = RandomField(seed=True,
+        random_field1 = RandomField(seed=True,
                                    lacor=10.,
                                     xgrid=np.linspace(0., length, 600),
                                     nsim=1,
                                     loc=.0,
-                                    shape=15.,
-                                    scale=6.5,
+                                    shape=21.,
+                                    scale=5.8 * 1.1,
                                     non_negative_check=True,
                                     distribution='Weibull'
                                    )
 
         rf = CBEMClampedFiberStressSP()
-        rand = FunctionRandomization(q=rf,
+        rand1 = FunctionRandomization(q=rf,
                                      tvars=dict(tau=tau,
                                                 l=l,
                                                 E_f=Ef,
@@ -299,21 +291,32 @@ if __name__ == '__main__':
 
         scm1 = SCM(length=length,
                   nx=nx,
-                  random_field=random_field,
-                  cb_randomization=rand,
+                  random_field=random_field1,
+                  cb_randomization=rand1,
                   cb_type='mean',
                   load_sigma_c_min=.1,
                   load_sigma_c_max=27.,
                   load_n_sigma_c=200,
                   n_w=60,
                   n_x=81,
-                  n_BC=3
+                  n_BC=2
                   )
 
         scm_view1 = SCMView(model=scm1)
         scm_view1.model.evaluate()
 
-        rand = FunctionRandomization(q=rf,
+        random_field2 = RandomField(seed=True,
+                                   lacor=10.,
+                                    xgrid=np.linspace(0., length, 600),
+                                    nsim=1,
+                                    loc=.0,
+                                    shape=21.,
+                                    scale=5.8,
+                                    non_negative_check=True,
+                                    distribution='Weibull'
+                                   )
+
+        rand2 = FunctionRandomization(q=rf,
                                      tvars=dict(tau=tau,
                                                 l=l,
                                                 E_f=Ef,
@@ -329,15 +332,15 @@ if __name__ == '__main__':
 
         scm2 = SCM(length=length,
                   nx=nx,
-                  random_field=random_field,
-                  cb_randomization=rand,
+                  random_field=random_field2,
+                  cb_randomization=rand2,
                   cb_type='mean',
                   load_sigma_c_min=.1,
                   load_sigma_c_max=25.,
                   load_n_sigma_c=200,
                   n_w=60,
                   n_x=81,
-                  n_BC=3
+                  n_BC=2
                   )
 
         scm_view2 = SCMView(model=scm2)
@@ -362,4 +365,6 @@ if __name__ == '__main__':
         #plt.legend(loc='best')
         plt.show()
 
-    #plot()
+    plot()
+    #opt = Opt()
+    #opt.get_parameters()
