645a96fd5752bde6613adfc42ac1371a2d3739ae
rostar <rrypl@.imb.rwth-aachen.de>
2012-11-02 18:49:49 +0100

    analyt formulas for mean length of broken fibers

--- scratch/scratching.py
+++ scratch/scratching.py
@@ -3,29 +3,39 @@ from scipy.optimize import fsolve
 from scipy.stats import weibull_min
 from matplotlib import pyplot as plt
 from scipy.optimize import broyden2
+from scipy.special import gammainc
 
 m = 5.0
 s = 0.02
 Ef = 72e3
 L0 = 50.
+T = 0.02/50.
+
+def P(e):
+    return 1. - np.exp(-(e/s)**m)
 
 def Pf(e, L):
     return 1. - np.exp(-L/L0*(e/s)**m)
 
 def Pf2(e, a):
     f = a * (e/s)**m / (m+1)
-    print 'f=', f
     return 1. - np.exp(-2. / L0 * f)
 
 def Pf3(e, tau, l):
     f = (((tau * l - 1)*((e*(1-tau*l)))/s)**m + (e/s)**m) / (m+1) / tau
-    print 'f=', f
     return 1. - np.exp(-2. / L0 * f)
 
-print Pf(0.02, 50.)
-print Pf2(0.02, 50.)
-print Pf3(0.02, 1./50., 5.)
+def muL(L, e):
+    nom = L * e ** 2 * gammainc(2./m, (e/s)**m)/m/s**2 + L**2/2.
+    de = L * e **2 * gammainc(1./m, (e/s)**m)/m/s + L
+    return nom/de
+    
+#print Pf(0.02, 50.)
+#print Pf2(0.02, 50.)
+#print Pf3(0.02, 1. / 50., 5.)
 
-#L_arr = np.linspace(0, 200, 100)
-#plt.plot(L_arr, Pf(L_arr, 0.02))
+e_arr = np.linspace(0, 0.02, 1000)
+L_arr = np.linspace(0, 50., 1000)
+print np.sum(P(0.02 - e_arr) * L_arr) / np.sum(P(0.02 - e_arr))
+print muL(50., 0.02)
 #plt.show()
