fdaf9a147d748453e30ef7467de47ba3de702238
rostar <rrypl@.imb.rwth-aachen.de>
2012-12-17 11:52:46 +0100

    mu L

--- scratch/scratching.py
+++ scratch/scratching.py
@@ -9,17 +9,19 @@ m = 5.
 s = 0.02
 L0 = 100.
 
-
+# failure probability between 0 and a
 def CDFa(e):
     T = 2. * tau / r / Ef
     a = e / T
     return 1. - np.exp(-a * (e / s) ** m / (m + 1) / L0)
 
+# failure probability density between 0 and a
 def PDFa(e):
     T = 2. * tau / r / Ef
     a = e / T
-    return np.exp(-a * (e / s) ** m / (m + 1) / L0) * ((e/s)**m/T/L0)
+    return np.exp(-a * (e / s) ** m / (m + 1) / L0) * ((e / s) ** m * m / (T * L0 * (m + 1)))
 
+# failure probability between 0 and L
 def CDFL(e, L):
     T = 2. * tau / r / Ef
     a = e / T
@@ -40,26 +42,30 @@ def mu_L(e_arr):
         muL.append(scalar_mu_L(e))
     return np.array(muL)
 
-def scalar_mu_L_rostar(e):
+def scalar_mu_L_rr(e):
     e_arr = np.linspace(0.0001, e, 300)
     muL_arr = mu_L(e_arr)
     PDF = PDFa(e_arr)
+    plt.figure()
+    plt.plot(e_arr, PDF)
+    plt.show()
     c = 1. / CDFa(e)
+    print np.trapz(PDF, e_arr) / c
     integ = np.trapz(PDF * muL_arr, e_arr)
     return c * integ
 
-def mu_L_rostar(e_arr):
+def mu_L_rr(e_arr):
     muL = []
     for e in e_arr:
-        muL.append(scalar_mu_L_rostar(e))
+        muL.append(scalar_mu_L_rr(e))
     return np.array(muL)
 
-e_arr = np.linspace(0.0001, 0.1, 200)
+e_arr = np.linspace(0.0001, 0.1, 20)
 plt.plot(e_arr, CDFa(e_arr)/np.max(CDFa(e_arr)) * 10, label='CDF')
 #plt.plot(e_arr, PDFa(e_arr), label='PDFa')
 #plt.plot(e_arr, CDFL(e_arr, 2.))
 plt.plot(e_arr, mu_L(e_arr), label='mu_L Curtin')
-plt.plot(e_arr, mu_L_rostar(e_arr), label='mu_L Rypl')
+plt.plot(e_arr, mu_L_rr(e_arr), label='mu_L RR')
 plt.xlabel('eps crack')
 plt.ylabel('mu_L')
 #plt.plot(e_arr, e_arr / 2. / tau * r * Ef, label='a')
