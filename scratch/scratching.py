74a250b8efba37cdd16a390e435e460b02236586
rostar <rrypl@.imb.rwth-aachen.de>
2013-01-22 17:11:53 +0100

    scratch

--- scratch/scratching.py
+++ scratch/scratching.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import weibull_min
 from scipy.optimize import brentq
+from scipy.integrate import cumtrapz
 
 def H(x):
     return x >= 0.0
@@ -20,13 +21,16 @@ def CDFw(e, L):
 def CDFa(e):
     T = 2. * tau / r / Ef
     a = e / T
-    return 1. - np.exp(-a * (e / s) ** m / (m + 1) / L0)
+    return 1. - np.exp(-a * 2 * (e / s) ** m / (m + 1) / L0)
 
 # failure probability density between 0 and a at e
 def PDFa(e):
     T = 2. * tau / r / Ef
     a = e / T
-    return np.exp(-a * (e / s) ** m / L0 / (m + 1)) * (e / s) ** m / (T * L0)
+    return np.exp(-a * 2 * (e / s) ** m / L0 / (m + 1)) * 2 * (e / s) ** m / (T * L0)
+
+e_arr = np.linspace(0.001, 0.05, 100)
+plt.plot(e_arr[1:], cumtrapz(PDFa(e_arr), e_arr))
 
 # failure probability between 0 and L at e
 def CDF_L(e, L):
@@ -34,110 +38,6 @@ def CDF_L(e, L):
     a = e / T
     Pf = 1. - np.exp(-a * (e / s) ** m * (1 - (1 - L / a) ** (m + 1)) / (m + 1) / L0)
     return Pf / CDFa(e)
-
-# failure probability for dx at x given failure at e
-def Pf_dx_L(e, L, dx):
-    T = 2. * tau / r / Ef
-    a = e / T
-    
-    CDF = np.exp((a * (e / s) ** m * (1 - L / a) ** (m + 1)) / (m + 1) / L0)
-    return CDF
-
-#l_arr = np.linspace(0.0, 80., 1000)
-#plt.plot(l_arr, CDFL2(0.02, l_arr))
-#plt.show()
-
-# failure probability density at L, e
-def PDFLgiven_e(e, L):
-    T = 2. * tau / r / Ef
-    a = e / T
-    pdf = m * dx * (1 - L/a) / L0 / s / a * (e * (1.-L/a)/s)**(m-1)
-    return pdf * H(a - L)
-
-# failure probability density at L, e
-def PDFL(e, L, dx):
-    T = 2. * tau / r / Ef
-    a = e / T
-    pdf = (1. - CDFa(e)) * m * dx * (1 - L/a) / L0 / s / a * (e * (1.-L/a)/s)**(m-1)
-    return pdf * H(a - L)
-
-#e_arr = np.linspace(0.0001, 0.05, 500).reshape(500,1)
-#l_arr = np.linspace(0., 60., 200).reshape(1, 200)
-#
-##print np.trapz(PDFL(e_arr, l_arr), e_arr, axis = 0)
-#PdF = PDFL(e_arr, l_arr, 60./200.)
-#p = np.trapz(PdF, e_arr, axis = 0).flatten()
-#P = np.trapz(p, l_arr.flatten())
-#print P
-#plt.plot(l_arr.flatten(), np.trapz(PDFL(e_arr, l_arr), e_arr, axis = 0).flatten())
-#plt.show()
-
-def muL(e):
-    T = 2. * tau / r / Ef
-    a = e / T
-    L_arr = np.linspace(0., a, 100)
-    pdfL = PDFL(e, L_arr) / CDFa(e)
-    mul = np.trapz(pdfL * L_arr, L_arr)
-    return mul
-
-def muLate(e):
-    muL_lst = []
-    e_arr = np.linspace(0.0001, e, 100)
-    for ei in e_arr:
-        muL_lst.append(muL(ei))
-    muL_arr = np.array(muL_lst)
-    PDFa_arr = PDFa(e_arr) / (CDFa(e))
-    muLate_value = np.trapz(muL_arr * PDFa_arr, e_arr)
-    return muLate_value
-
-def muLate_arr(e_arr):
-    muLate_lst = []
-    for eii in e_arr:
-        muLate_lst.append(muLate(eii))
-    return np.array(muLate_lst)
-
-def scalar_mu_L(e):
-    c = 1. / CDFa(e)
-    T = 2. * tau / r / Ef
-    a = e / T
-    z_arr = np.linspace(0, a, 300)
-    integ = np.trapz(CDFL2(e, z_arr), z_arr)
-    z_arr * PDFa(e)
-    return a - c * integ
-
-def mu_L(e_arr):
-    muL = []
-    for e in e_arr:
-        muL.append(scalar_mu_L(e))
-    return np.array(muL)
-
-def mu_L_at_e(e):
-    T = 2. * tau / r / Ef
-    a = e / T
-    z_arr = np.linspace(0.0, a, 50)
-    z_arr_1 = np.hstack((0.0, z_arr[:-1]))
-    z_arr_2 = np.hstack((z_arr[1:], z_arr[-1]))
-    integ = np.trapz(z_arr * (1 - CDFL(e, z_arr_1)) * CDFw(e - T*z_arr, z_arr[1]-z_arr[0]) * (1. - CDFa(e - T*z_arr_2)), z_arr)
-    c = 1. / np.trapz((1 - CDFL(e, z_arr_1)) * CDFw(e - T * z_arr, z_arr[1]-z_arr[0]) * (1. - CDFa(e - T*z_arr_2)), z_arr)
-    return c * integ
-
-def scalar_mu_L_rr(e):
-    e_arr = np.linspace(0.0001, e, 100)
-    muL = []
-    for ei in e_arr:
-        muL.append(mu_L_at_e(ei))
-    muL_arr = np.array(muL)
-    PDF = PDFa(e_arr)
-    c = 1. / CDFa(e)
-    integ = np.trapz(PDF * muL_arr, e_arr)
-    return c * integ
-
-def mu_L_rr(e_arr):
-    muL = []
-    for e in e_arr:
-        muL.append(scalar_mu_L_rr(e))
-        #muL.append(mu_L_at_e(e))
-    return np.array(muL)
 
 def simulation(e):
     T = 2. * tau / r / Ef
@@ -167,33 +67,48 @@ def MC(n, e_arr):
         L.append(np.mean(np.array(ls)))
     return L
 
-from math import pi
-from scipy.special import gamma, gammainc
+from scipy.optimize import fsolve
 
-def evans():
-    s = 0.02 * 200e3
-    suma = (L0 * s**m * tau * (m+1)/r)**(1./(m+1))
-    h = L0/2. * (s/suma)**m * gamma((m+2)/(m+1))
-    return h
+def MC_CDFa(n, e_arr):
+    nx = 500.
+    emax = e_arr[-1]
+    T = 2. * tau / r / Ef
+    amax = emax / T
+    dx = amax/nx
+    z_arr = np.linspace(amax/2./nx, amax - amax/2./nx, nx)
+    eu = []
+    def residuum(ef0, fu):
+        return np.min(fu - ef0 + T * z_arr)
+    for i in range(n):
+        fu_arr = weibull_min(m, scale=s * (dx/L0)**(-1./m)).ppf(np.minimum(np.random.rand(nx),
+                                                                       np.random.rand(nx)))
+        eu.append(fsolve(residuum, 0.001, args=(fu_arr)))
+    
+    cdf = []
+    for ei in e_arr:
+        print np.sum(np.array(eu) < ei)
+        cdf.append(np.sum(np.array(eu) < ei))
+    return np.array(cdf)/float(n)
+    
 
-def evans_arr(e):
-    s = 0.02 * 200e3
-    suma = ((L0 * s**m * tau * (m+1))/r)**(1./(m+1))
-    alpha = (e * 200e3/suma)**(m+1)
-    h = L0/2. * (s/suma) ** m * gammainc((m+2)/(m+1), alpha)
-    return h
+def PMF(e, l_arr):
+    T = 2. * tau / r / Ef
+    a = e / T
+    dx = l_arr[1]
+    PMF = np.exp(dx/L0*(e * (1-l_arr/a)/s)**m -a * (e / s) ** m / (m + 1) / L0)
+    return PMF / np.sum(PMF)
 
-l_arr = np.linspace(0.0, 60., 100)
-e_arr = np.linspace(0.001, 0.07, 100)
+def MUL_at_e(e):
+    T = 2. * tau / r / Ef
+    a = e / T
+    l_arr = np.linspace(0.0, a, 300)
+    return np.trapz(PMF(e, l_arr) * l_arr, l_arr)
+
+l_arr = np.linspace(0.0, 90., 50)
+e_arr = np.linspace(0.001, 0.05, 100)
 e_arr2 = np.linspace(0.015, 0.07, 20)
-#plt.plot(e_arr, evans_arr(e_arr), label='Evans')
-#plt.plot(e_arr, muLate_arr(e_arr), label='new')
-plt.plot(e_arr2, MC(50000, e_arr2), 'ro', label='MC')
-#plt.plot(e_arr, CDFa(e_arr) / np.max(CDFa(e_arr)) * 10, label='10xCDF')
-#plt.plot(e_arr, PDFa(e_arr), label='PDFa')
-#plt.plot(l_arr, PDFL(0.02, l_arr), label='PDFL')
-#plt.plot(e_arr, mu_L(e_arr), label='mu_L Curtin')
-#plt.plot(e_arr, mu_L_rr(e_arr), label='mu_L RR')
+plt.plot(e_arr, MC_CDFa(5000, e_arr))
+plt.plot(e_arr, CDFa(e_arr))
 plt.xlabel('eps crack')
 plt.ylabel('mu_L')
 #plt.plot(e_arr, e_arr / 2. / tau * r * Ef, label='a')
