a40eb2003c245d6566dd8a97ca241d95673661fb
rostar <rrypl@.imb.rwth-aachen.de>
2013-01-24 16:02:14 +0100

    mean pullout length evaluated and implemented in the mean crack bridge force with rigid matrix

--- scratch/CB/independent_fibers/indep_CB_model.py
+++ scratch/CB/independent_fibers/indep_CB_model.py
@@ -30,6 +30,7 @@ from stats.spirrid.rf import \
 
 from matplotlib import pyplot as plt
 from math import pi
+from scipy.special import gammainc, gamma
 
 
 def H(x):
@@ -73,9 +74,6 @@ class CBResidual(RF):
     V_f = Float(0.0175, auto_set=False, enter_set=True, input=True,
               distr=['uniform'])
 
-    Pf = Float(0.0, auto_set=False, enter_set=True, input=True,
-              distr=['uniform'])
-
     w = Float(auto_set=False, enter_set=True, input=True,
                distr=['uniform'], desc='crack width',
                ctrl_range=(0.0, 1.0, 10))
@@ -89,33 +87,32 @@ class CBResidual(RF):
 
     C_code = Str('')
 
-    def omega(self, f_inf, w, T, E_f, r, m, l0, s0, Pf):
-        epsf0 = f_inf / (pi * r ** 2) / E_f
-        a = epsf0 / T
-        PfL = 1 - np.exp(-(a * (epsf0 / s0) ** m) / (l0 * (m + 1)))
-        PDF = (1./l0) * (epsf0 * (1 - x / a)/s0) ** m * np.exp(-(a * (epsf0 / s0) ** m) / (l0 * (m + 1)))
-        muL = np.trapz(x * PDF, x)
-        return PfL, muL
-
-    def __call__(self, w, tau, E_f, V_f, r, m, l0, s0, Pf):
+    def __call__(self, w, tau, E_f, V_f, r, m, l0, s0):
         #defining variables
         T = 2. * tau / r
-        f_inf = np.sqrt(T * w / E_f) * pi * r ** 2 * E_f * np.ones_like(Pf)
-        omega, muL = self.omega(f_inf, w, T, E_f, r, m, l0, s0, Pf)
-        f = f_inf * (1 - omega) + muL * 2 * pi * r * tau * omega
-        return f
+        ef0_inf = np.sqrt(T * w / E_f)
+        a_inf = ef0_inf * E_f / T
+        Pf = 1. - np.exp(-a_inf * 2 * (ef0_inf / s0) ** m / (m + 1) / l0)
+
+        psi = (l0/r*(s0* E_f)**m*tau*(m+1))**(1./(m+1.))
+        alpha = (ef0_inf * E_f / psi)**(m+1)
+        muL_failed = l0/2. * ((s0* E_f)/psi)**m * gamma((m+2)/(m+1)) * gammainc((m+2)/(m+1), alpha) / Pf
+        ef0_failed = T * muL_failed / E_f
+
+        ef0_tot = ef0_inf * (1-Pf) + ef0_failed * Pf
+        return ef0_tot
 
 from stats.spirrid.spirrid import SPIRRID
 from stats.spirrid.rv import RV
 
 
-def CB_composite_stress(w, tau, E_f, V_f, r, m, l0, s0, Pf, n_int):
+def CB_composite_stress(w, tau, E_f, V_f, r, m, l0, s0, n_int):
     cb = CBResidual()
     s = SPIRRID(q=cb,
                 sampling_type='PGrid',
                 evars=dict(w=w),
                 tvars=dict(tau=tau, E_f=E_f, V_f=V_f, r=r,
-                           m=m, l0=l0, s0=s0, Pf=Pf),
+                           m=m, l0=l0, s0=s0),
                 n_int=n_int)
 
     if isinstance(r, RV):
@@ -126,16 +123,15 @@ def CB_composite_stress(w, tau, E_f, V_f, r, m, l0, s0, Pf, n_int):
     sigma_c = s.mu_q_arr * V_f / (pi * Er)
     plt.plot(w, sigma_c, color='blue', label='SPIRRID')
     plt.xlabel('w [mm]')
-    plt.ylabel('sigma_c [MPa]')
+    plt.ylabel('epsfmax [-]')
     plt.show()
 
 if __name__ == '__main__':
-    w = np.linspace(0, 3.0, 100)
-    tau = RV('uniform', loc=0.01, scale=.10)
+    w = np.linspace(0, 5.0, 500)
+    tau = RV('uniform', loc=0.01, scale=.50)
     E_f = 72e3
     V_f = 0.1
     r = RV('uniform', loc=0.002, scale=0.004)
-    Pf = RV('uniform', loc=0.0, scale=1.0)
     m = 5.
     l0 = 100.
     s0 = 0.017
@@ -143,4 +139,5 @@ if __name__ == '__main__':
     #cb = CBResidual()
     #plt.plot(w, cb(w, 0.5, E_f, V_f, r, m, l0, s0, Pf) / (pi * r ** 2))
     #plt.show()
-    CB_composite_stress(w, tau, E_f, V_f, r, m, l0, s0, Pf, n_int)
+    CB_composite_stress(w, tau, E_f, V_f, r, m, l0, s0, n_int)
+
--- scratch/scratching.py
+++ scratch/scratching.py
@@ -11,7 +11,7 @@ Ef = 200e3
 r = 0.003
 tau = 0.1
 m = 5.
-s = 0.02
+s = 0.03
 L0 = 100.
 
 def CDFw(e, L):
@@ -38,68 +38,75 @@ def CDF_L(e, L):
 
 def h(e, de, x, dx):
     a = e / 2. / tau * r * Ef
-    s_mod = s * (100. / 1.196143) ** (1./m) #0.04847
-    h_xe = (1 - CDFa(e)) * H(a - x) * e ** (m - 1) * 2 * (1. - x/a) ** (m) * m / s_mod ** m
-    return h_xe * x * dx * de
+    h_xe = (1 - CDFa(e)) * H(a - x) / s ** m * 2 * (e * (1. - x/a)) ** (m-1) * m # * e ** (m - 1) * 2 * ((1. - x/a)/s) ** (m) * m
+    return h_xe / L0
 
 from scipy.optimize import fsolve
+from scipy.special import gammainc, gamma
 
-def find_s():
-    def residuum(c):
-        e_arr = np.linspace(0.0001, 0.04, 2000).reshape(2000, 1)
-        x_arr = np.linspace(0.0, 80., 2000).reshape(1, 2000)
-        mu_q = h(e_arr, 0.04/2000., x_arr, 80. / 2000., c)
-        return np.sum(mu_q) - 1.0
-    print fsolve(residuum, 0.02)
+def evans(T):
+    suma = (L0/r*(s* Ef)**m*tau*(m+1))**(1./(m+1.))
+    alpha = (T/suma)**(m+1)
+    return L0/2. * ((s* Ef)/suma)**m * gamma((m+2)/(m+1)) * gammainc((m+2)/(m+1), alpha) / CDFa(T/Ef)
 
-#find_s()
+e_arr = np.linspace(0.001, 0.05, 100)
+plt.plot(e_arr, evans(e_arr * Ef))
+
 from stats.spirrid import make_ogrid as orthogonalize
 from mayavi import mlab
-e_arr = np.linspace(0.0001, 0.04, 200).reshape(200, 1)
-x_arr = np.linspace(0.0, 80., 200).reshape(1, 200)
-mu_q = h(e_arr, 0.04/200., x_arr, 80. / 200.)
-print np.sum(mu_q)
-ctrl_arr = orthogonalize([np.arange(len(e_arr.flatten())), np.arange(len(x_arr.flatten()))])
-mlab.surf(e_arr[0], e_arr[1], mu_q * 50000.)
-mlab.show()
-#mu_q = h(e_arr, 0.04/200., x_arr, 60. / 300.)
-#pdf_x = np.trapz(mu_q, e_arr.flatten(), axis = 0)
+nx = 200
+ne = 200
+e_arr = np.linspace(0.0001, 0.04, nx)
+x_arr = np.linspace(0.0, 80., ne)
+dx = x_arr[1] - x_arr[0]
+de = e_arr[1] - e_arr[0]
+h = h(e_arr.reshape(ne, 1), de, x_arr.reshape(1, nx), dx)
+pdf_x = np.trapz(h, e_arr.flatten(), axis=0)
+print 'sum = ', np.trapz(pdf_x.flatten(), x_arr.flatten())
+print 'mu_L =', np.trapz(pdf_x.flatten() * x_arr, x_arr.flatten())
+#ctrl_arr = orthogonalize([np.arange(len(e_arr.flatten())), np.arange(len(x_arr.flatten()))])
+#mlab.surf(e_arr[0], e_arr[1], h / np.max(h) * nx)
+#mlab.show()
 #plt.plot(x_arr.flatten(), pdf_x.flatten())
-#print np.trapz(pdf_x.flatten(), x_arr.flatten())
 #plt.show()
 
-def simulation(e):
+def simulation(e_max):
     T = 2. * tau / r / Ef
-    a = e / T
+    a = e_max / T
     nz = 100
     z_arr = np.linspace(a/2./nz, a - a/2./nz, nz)
-    eps = e - T * z_arr
+    eps = e_max - T * z_arr
     dz = a/nz
     fu_arr = weibull_min(m, scale=s * (dz/L0)**(-1./m)).ppf(np.minimum(np.random.rand(nz),
                                                                        np.random.rand(nz)))
     max_diff = np.max(eps - fu_arr)
     if max_diff > 0.0:
-        return z_arr[np.argmax(eps - fu_arr)]
+        return z_arr[np.argmax(eps - fu_arr)], e_max - max_diff 
     else:
-        return None
+        return None, None
 
 def MC(n, e_arr):
-    L = []
-    for e in e_arr:
-        ls = []
-        for i in range(n):
-            l = simulation(e)
-            if l == None:
-                pass
-            else:
-                ls.append(l)
-        L.append(np.mean(np.array(ls)))
-    return L
+    Ls = []
+    es = []
+    for i in range(n):
+        l, e = simulation(e_arr[-1])
+        if l == None:
+            pass
+        else:
+            Ls.append(l)
+            es.append(e)
+    es = np.array(es)
+    Ls = np.array(Ls)
+    muL = []
+    for ei in e_arr:
+        mask = es < ei
+        muL.append(np.mean(Ls[mask]))
+    return muL
 
 from scipy.optimize import fsolve
 
 def MC_CDFa(n, e_arr):
-    nx = 500.
+    nx = 1000.
     emax = e_arr[-1]
     T = 2. * tau / r / Ef
     amax = emax / T
@@ -112,30 +119,18 @@ def MC_CDFa(n, e_arr):
         fu_arr = weibull_min(m, scale=s * (dx/L0)**(-1./m)).ppf(np.minimum(np.random.rand(nx),
                                                                        np.random.rand(nx)))
         eu.append(fsolve(residuum, 0.001, args=(fu_arr)))
-    
+
     cdf = []
     for ei in e_arr:
         cdf.append(np.sum(np.array(eu) < ei))
     return np.array(cdf)/float(n)
-    
-
-def PMF(e, l_arr):
-    T = 2. * tau / r / Ef
-    a = e / T
-    dx = l_arr[1]
-    PMF = np.exp(dx/L0*(e * (1-l_arr/a)/s)**m -a * (e / s) ** m / (m + 1) / L0)
-    return PMF / np.sum(PMF)
-
-def MUL_at_e(e):
-    T = 2. * tau / r / Ef
-    a = e / T
-    l_arr = np.linspace(0.0, a, 300)
-    return np.trapz(PMF(e, l_arr) * l_arr, l_arr)
 
 l_arr = np.linspace(0.0, 90., 50)
 e_arr = np.linspace(0.001, 0.05, 100)
 e_arr2 = np.linspace(0.015, 0.07, 20)
-plt.plot(e_arr, CDFa(e_arr))
+mc = MC(10000, e_arr)
+print mc[-1]
+plt.plot(e_arr, mc, 'ro')
 plt.xlabel('eps crack')
 plt.ylabel('mu_L')
 plt.legend(loc='best')
