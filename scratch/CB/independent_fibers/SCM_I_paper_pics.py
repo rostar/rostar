18a5c8eea9787c707e6e4776789de516f444396e
rostar <rrypl@.imb.rwth-aachen.de>
2013-02-04 18:48:58 +0100

    paper pics

--- scratch/CB/independent_fibers/SCM_I_paper_pics.py
+++ scratch/CB/independent_fibers/SCM_I_paper_pics.py
@@ -12,7 +12,7 @@ import numpy as np
 E_f = 200e3
 V_f = 0.01
 r = 0.003
-tau = 1.3
+tau = .3
 #m = 5.
 s = 0.015
 L_0 = 100.
@@ -25,10 +25,10 @@ def median_strength_vs_T(T_arr, m_arr):
     for mi in m_arr:
         median_eps_scaling = (-0.5 * np.log(1. - p) * T_arr / E_f * L_0 * (mi+1) * s**mi) ** (1./(mi+1)) 
         a_xi = median_eps_scaling * E_f / T_arr
-        plt.plot(a_xi, median_eps_scaling, lw=2, label='m = ' + str(mi))
+        plt.plot(T_arr, median_eps_scaling, lw=2, label='m = ' + str(mi))
     a_xi = np.linspace(10., 100., 50)
     anal = (s**2.0 * L_0 * (3.0) * np.log(2) / 2. / a_xi)**(0.5)
-    plt.plot(a_xi, anal, 'ro')
+    #plt.plot(a_xi, anal, 'ro')
     plt.legend(loc='best')
     plt.show()
 
@@ -45,21 +45,16 @@ def random_xi(w_arr, m_arr):
         ni = mi+1
         si = (xi_ref * ni / gamma(1./ni))**(ni/mi) * (2 * E_f/ni/L_0/T)**(1./mi)
         CDF = 1. - np.exp(-xi_ref * E_f / T * 2 * (xi_ref / si) ** mi / ni / L_0)
-        spirrid = SPIRRID(q=CBResidual(include_pullout=True),
-            sampling_type='PGrid',
-            evars=dict(w=w_arr),
-            tvars=dict(tau=tau, E_f=E_f, V_f=V_f, r=r,
-                       m=mi, L_0=L_0, s=si, Pf=CDF),
-            n_int=500)
-        sigma_c = spirrid.mu_q_arr / r**2
-        plt.plot(w_arr, sigma_c, label='determ m = ' + str(mi))
+        cb = CBResidual(include_pullout=False)
+        determ = cb(w_arr, tau, E_f, V_f, r, mi, L_0, si, CDF) / r**2
+        plt.plot(w_arr, determ, label='determ m = ' + str(mi))
        
         spirrid = SPIRRID(q=CBResidual(include_pullout=True),
                     sampling_type='PGrid',
                     evars=dict(w=w_arr),
                     tvars=dict(tau=tau, E_f=E_f, V_f=V_f, r=r,
                                m=mi, L_0=L_0, s=si, Pf=Pf),
-                    n_int=500)
+                    n_int=5000)
         if isinstance(r, RV):
             r_arr = np.linspace(r.ppf(0.001), r.ppf(0.999), 300)
             Er = np.trapz(r_arr ** 2 * r.pdf(r_arr), r_arr)
@@ -75,8 +70,8 @@ def random_xi(w_arr, m_arr):
  
 
 T_arr = 2 * np.linspace(0.05, .7, 100) / r
-m_arr = np.array([.5, 2.0, 10.])
-w_arr = np.linspace(0, 1.0, 300)
+m_arr = np.array([1., 7.0, 15.])
+w_arr = np.linspace(0, .7, 300)
 #median_strength_vs_T(T_arr, m_arr)
 random_xi(w_arr, m_arr)
 #from scipy.stats import weibull_min
@@ -102,5 +97,4 @@ random_xi(w_arr, m_arr)
 ##plt.plot(l_arr, mu3, lw=2)
 #print 100. * np.log(2)
 #plt.ylim(0,0.2)
-#plt.show()  
-        
+#plt.show()
