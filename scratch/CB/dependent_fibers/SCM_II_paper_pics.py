c8b627c80aae18b31f60aa577838698225600be3
rostar <erix@post.cz>
2013-05-29 00:12:27 +0200

    paper pics

--- scratch/CB/dependent_fibers/SCM_II_paper_pics.py
+++ scratch/CB/dependent_fibers/SCM_II_paper_pics.py
@@ -20,19 +20,20 @@ from spirrid.rv import RV
 from quaducom.meso.homogenized_crack_bridge.elastic_matrix.reinforcement import Reinforcement, WeibullFibers
 
 reinf = Reinforcement(r=0.01,#RV('uniform', loc=0.001, scale=0.005),
-                      tau=RV('uniform', loc=.1, scale=1.),
-                      V_f=0.3,
+                      tau=0.1,
+                      V_f=0.01,
                       E_f=200e3,
-                      xi=WeibullFibers(shape=5., sV0=0.02),
+                      xi=1000.,
                       n_int=50,
                       label='reinforcement')
 
 model = CompositeCrackBridge(E_m=25e3,
                              reinforcement_lst=[reinf],
-                             Ll=50.,
-                             Lr=50.)
+                             Ll=np.infty,
+                             Lr=np.infty)
 
 ccb_view = CompositeCrackBridgeView(model=model)
+w_arr = np.linspace(0.0, 1.0, 200)
 
 
 def ld_rigid_vs_el_mtrx():
@@ -126,112 +127,27 @@ def profiles_rigid_vs_el_mtrx():
     plt.ylim(0)
     plt.ylabel('$\epsilon$ [-]')
 
-def sigma_f(w_arr):
-    sf_arr = ccb_view.sigma_f_lst(w_arr)
-    #for i, reinf in enumerate(ccb_view.model.reinforcement_lst):
-    #    plt.plot(w_arr, sf_arr[:, i], label=reinf.label)
-    plt.plot(w_arr, sf_arr[:,0], ls='dashed', label='$\sigma_{\mathrm{f}1}$', color='black')
-    plt.plot(w_arr, sf_arr[:,1], ls='dotted', label='$\sigma_{\mathrm{f}2}$', color='black')
-    plt.xlabel('crack opening w [mm]')
-    plt.ylabel('stress [MPa]')
-    plt.ylim(0)
+def Vf_k(k):
+    Em = ccb_view.model.E_m
+    Ef = ccb_view.model.reinforcement_lst[0].E_f
+    return -Em*k/(Ef*k-Em*k-Ef)
 
-def sigma_c_w(w_arr, r, tau, E_f, E_m, V_f, xi, n_int):
-    reinf = Reinforcement(r=r, tau=tau, E_f=E_f, V_f=V_f,
-                          xi=xi, n_int=n_int)
-    model = CompositeCrackBridge(E_m=E_m,
-                             reinforcement_lst=[reinf],
-                             Ll=1000.,
-                             Lr=1000.)
-    ccb_view = CompositeCrackBridgeView(model=model)
-    sigma_c = []
-    for w in w_arr:
-        ccb_view.model.w = w
-        sigma_c.append(ccb_view.sigma_c)
-    plt.plot(w_arr, sigma_c, lw=2, label='elastic_mtrx')
+def sigma_c_arr():
+    Vf = Vf_k(0.1)
+    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
+    sigma = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr, sigma / Vf, lw=2)
+    Vf = Vf_k(0.5)
+    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
+    sigma = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr, sigma / Vf, lw=2)
+    Vf = Vf_k(0.8)
+    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
+    ccb_view.model.reinforcement_lst = [reinf]
+    sigma = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr, sigma / Vf, lw=2)
 
-    cb = CBRigidMatrix()
-    spirrid = SPIRRID(q=cb, sampling_type='PGrid',
-                      evars=dict(w=w_arr),
-                      tvars=dict(r=r, tau=tau, E_f=E_f, V_f=V_f,
-                                 xi=xi),
-                      n_int=n_int)
-    plt.plot(w_arr, E_f * V_f * spirrid.mu_q_arr, lw=2, label='rigid_mtrx')
-
-def errors(k_ratio):
-    sigmax_el = []
-    wmax = []
-    for k in k_ratio:
-        E_m = E_f * V_f / k / (1 - V_f)
-        xi = WeibullFibers(shape=5., scale=0.02, L0=10.)#RV('weibull_min', shape=50., scale=.02)
-        r = 0.002#RV('uniform', loc=0.002, scale=.002)
-        tau = RV('weibull_min', shape=80., scale=.5)
-        reinf = Reinforcement(r=r, tau=tau, V_f=0.1, E_f=200e3, xi=xi, n_int=20, label='carbon')
-        model = CompositeCrackBridge(E_m=E_m,
-                                 reinforcement_lst=[reinf],
-                                 Ll=100.,
-                                 Lr=100.)
-        ccb_view = CompositeCrackBridgeView(model=model)
-        sigmax_el.append(ccb_view.sigma_c_max[0])
-        wmax.append(ccb_view.sigma_c_max[1])
-    ccb_view.model.E_m = 10e10
-    rigid_sigma = ccb_view.sigma_c_max[0]
-    rigid_w = ccb_view.sigma_c_max[1]
-
-    plt.plot(k_ratio, np.array(sigmax_el)/rigid_sigma, label='$\sigma_\mathrm{c, max, el}/\sigma_\mathrm{c, max, rigid}$', lw=2, color='red')
-    plt.ylabel('$\sigma_\mathrm{c, max, el}/\sigma_\mathrm{c, max, rigid}$ and $w_\mathrm{max, el} / w_\mathrm{max, rigid}$')
-    plt.xlabel('$K_\mathrm{f}/K_\mathrm{m}$')
-    plt.title('effect of matrix stiffness')
-    plt.plot(k_ratio, np.array(wmax)/rigid_w, label='$w_\mathrm{max, el} / w_\mathrm{max, rigid}$', lw=2, color='blue')
-    plt.ylim(0, 1.3)
-
-def profile(wr, we, r, tau, E_f, E_m, V_f, xi, n_int):
-    reinf = Reinforcement(r=r, tau=tau, E_f=E_f, V_f=V_f,
-                          xi=xi, n_int=n_int)
-    model = CompositeCrackBridge(w=we,
-                                 E_m=E_m,
-                                 reinforcement_lst=[reinf],
-                                 Ll=1000., Lr=1000.)
-    ccb_view = CompositeCrackBridgeView(model=model)
-    plt.plot(ccb_view.x_arr, ccb_view.mu_epsf_arr, lw=2, label='el_mtrx_yarn')
-    plt.plot(ccb_view.x_arr, ccb_view.epsm_arr, lw=2, label='el_mtrx_mtrx')
-
-    cb = CBRigidMatrixSP()
-    x_arr = np.hstack((-np.linspace(3.*ccb_view.x_arr[-1], 0., 100),
-                        np.linspace(0.0, 3.*ccb_view.x_arr[-1],100)))
-    spirrid = SPIRRID(q=cb, sampling_type='PGrid',
-                      evars=dict(x=x_arr),
-                      tvars=dict(w=wr, r=r, tau=tau, E_f=E_f, V_f=V_f, xi=xi),
-                      n_int=n_int)
-    plt.plot(x_arr, spirrid.mu_q_arr, lw=2, label='rigid_mtrx_yarn')
-    plt.xlabel('position [mm]')
-    plt.ylabel('strain')
-
-def sigma_c_u(w_arr, r, tau, E_f, E_m, V_f, xi, n_int):
-    reinf = Reinforcement(r=r, tau=tau, E_f=E_f, V_f=V_f,
-                          xi=xi, n_int=n_int)
-    model = CompositeCrackBridge(E_m=E_m,
-                             reinforcement_lst=[reinf],
-                             Ll=30.,
-                             Lr=30.)
-    ccb_view = CompositeCrackBridgeView(model=model)
-    sigma_c = []
-    u = []
-    for w in w_arr:
-        ccb_view.model.w = w
-        u.append(ccb_view.u_evaluated)
-        sigma_c.append(ccb_view.sigma_c)
-    plt.plot(w_arr, sigma_c, lw=1, color='blue')
-    plt.plot(u, sigma_c, lw=1, color='red')
-
-    cb = CBRigidMatrix()
-    spirrid = SPIRRID(q=cb, sampling_type='PGrid',
-                      evars=dict(w=w_arr),
-                      tvars=dict(r=r, tau=tau, E_f=E_f, V_f=V_f,
-                                 xi=xi),
-                      n_int=n_int)
-    plt.plot(w_arr, E_f * V_f * spirrid.mu_q_arr, lw=2, color='black')
-
+sigma_c_arr()
 #ld_rigid_vs_el_mtrx()
 #profiles_rigid_vs_el_mtrx()
 #elastic_matrix(np.linspace(.0, .5, 100))
