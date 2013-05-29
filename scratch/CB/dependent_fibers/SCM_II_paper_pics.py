9b98f8aa479123e1194339071443d0200d760b27
rostar <rrypl@.imb.rwth-aachen.de>
2013-05-29 17:59:18 +0200

    pics

--- scratch/CB/dependent_fibers/SCM_II_paper_pics.py
+++ scratch/CB/dependent_fibers/SCM_II_paper_pics.py
@@ -11,22 +11,13 @@ Created on Nov 19, 2012
 
 from etsproxy.mayavi import mlab as m
 import numpy as np
-from scipy.stats import weibull_min
-from stats.spirrid import make_ogrid as orthogonalize
 from quaducom.meso.homogenized_crack_bridge.elastic_matrix.hom_CB_elastic_mtrx import CompositeCrackBridge
 from quaducom.meso.homogenized_crack_bridge.elastic_matrix.hom_CB_elastic_mtrx_view import CompositeCrackBridgeView
 from matplotlib import pyplot as plt
 from spirrid.rv import RV
 from quaducom.meso.homogenized_crack_bridge.elastic_matrix.reinforcement import Reinforcement, WeibullFibers
 
-reinf = Reinforcement(r=0.01,#RV('uniform', loc=0.001, scale=0.005),
-                      tau=0.1,
-                      V_f=0.01,
-                      E_f=200e3,
-                      xi=1000.,
-                      n_int=50,
-                      label='reinforcement')
-
+reinf = Reinforcement(r=0.01, tau=0.1, V_f=0.01, E_f=200e3, xi=1000., n_int=50)
 model = CompositeCrackBridge(E_m=25e3,
                              reinforcement_lst=[reinf],
                              Ll=np.infty,
@@ -36,150 +27,104 @@ ccb_view = CompositeCrackBridgeView(model=model)
 w_arr = np.linspace(0.0, 1.0, 200)
 
 
-def ld_rigid_vs_el_mtrx():
-    w_arr = np.linspace(0.0, 0.8, 200)
-    r = 0.003
-    tau = 0.3
-    V_f = 0.11
-    E_f = 200e3
-    n_int = 30
-    E_m = 25e3
-    xi = 0.02
-    Xxi = RV('weibull_min', shape=5., scale=.02)
-    Xr = RV('uniform', loc=.002, scale=.002)
-    Xtau = RV('uniform', loc=.02, scale=.98)
-    reinf1 = Reinforcement(r=r, tau=tau, V_f=V_f, E_f=E_f, xi=xi, n_int=n_int)
-    reinf2 = Reinforcement(r=r, tau=tau, V_f=V_f, E_f=E_f, xi=Xxi, n_int=100)
-    reinf3 = Reinforcement(r=Xr, tau=Xtau, V_f=V_f, E_f=E_f, xi=Xxi, n_int=15)
-    model1 = CompositeCrackBridge(E_m=E_m, reinforcement_lst=[reinf1], Ll=35., Lr=35.)
-    model2 = CompositeCrackBridge(E_m=E_m, reinforcement_lst=[reinf2], Ll=35., Lr=35.)
-    model3 = CompositeCrackBridge(E_m=E_m, reinforcement_lst=[reinf3], Ll=35., Lr=35.)
-    ccb_view1 = CompositeCrackBridgeView(model=model1)
-    ccb_view2 = CompositeCrackBridgeView(model=model2)
-    ccb_view3 = CompositeCrackBridgeView(model=model3)
-    sigma_c1, u1, sigma_c2, u2, sigma_c3, u3 = [], [], [], [], [], []
-    for w in w_arr:
-        ccb_view1.model.w = w/2
-        ccb_view2.model.w = w
-        ccb_view3.model.w = w
-        u1.append(ccb_view1.u_evaluated)
-        sigma_c1.append(ccb_view1.sigma_c)
-        u2.append(ccb_view2.u_evaluated)
-        sigma_c2.append(ccb_view2.sigma_c)
-        u3.append(ccb_view3.u_evaluated)
-        sigma_c3.append(ccb_view3.sigma_c)
-    plt.plot(w_arr / 2., sigma_c1, lw = 2, color='black', label='w')
-    plt.plot(w_arr, sigma_c2, lw = 2, color='black')
-    plt.plot(w_arr, sigma_c3, lw = 2, color='black')
-    plt.plot(u1, sigma_c1, ls='dashed', lw = 2, color='black', label='u')
-    plt.plot(u2, sigma_c2, ls='dashed', lw = 2, color='black')
-    plt.plot(u3, sigma_c3, ls='dashed', lw = 2, color='black')
-    plt.xlabel('w, u [mm]')
-    plt.ylabel('$\sigma_c$ [MPa]')
-
-def profiles_rigid_vs_el_mtrx():
-    r = 0.003
-    tau = 0.3
-    V_f = 0.11
-    E_f = 200e3
-    E_m = 25e10
-    Ll = 35.
-    Lr = 35.
-    Xxi = RV('weibull_min', shape=5., scale=.02)
-    Xr = RV('uniform', loc=.002, scale=.002)
-    Xtau = RV('uniform', loc=.02, scale=.98)
-    reinf2 = Reinforcement(r=Xr, tau=Xtau, V_f=V_f, E_f=E_f, xi=Xxi, n_int=15)
-    model2 = CompositeCrackBridge(E_m=E_m, reinforcement_lst=[reinf2], Ll=Ll, Lr=Lr)
-    ccb_view2 = CompositeCrackBridgeView(model=model2)
-
-    ccb_view2.sigma_c_max
-    x2 = np.hstack((-Ll, ccb_view2.x_arr, Lr))
-    mu_epsf2 = np.hstack((ccb_view2.mu_epsf_arr[0], ccb_view2.mu_epsf_arr, ccb_view2.mu_epsf_arr[-1]))
-    epsm2 = np.hstack((ccb_view2.epsm_arr[0], ccb_view2.epsm_arr, ccb_view2.epsm_arr[-1]))
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(x2, mu_epsf2, lw=2, color='black')
-    ax.plot(x2, epsm2, lw=2, color='black')
-    p = ax.fill_between(x2, epsm2, mu_epsf2, facecolor='none')
-    from matplotlib.patches import PathPatch
-    path = p.get_paths()[0]
-    p1 = PathPatch(path, fc="none", hatch="/")
-    ax.add_patch(p1)
-    p1.set_zorder(p.get_zorder()-0.1)
-    p = ax.fill_between(x2, mu_epsf2, facecolor='none')
-    path = p.get_paths()[0]
-    p1 = PathPatch(path, fc="none", hatch="\\")
-    ax.add_patch(p1)
-    p1.set_zorder(p.get_zorder()-0.1)
-    
-    reinf1 = Reinforcement(r=r, tau=tau, V_f=V_f, E_f=E_f, xi=Xxi, n_int=15)
-    model1 = CompositeCrackBridge(E_m=E_m, reinforcement_lst=[reinf1], Ll=Ll, Lr=Lr)
-    ccb_view1 = CompositeCrackBridgeView(model=model1)
-    ccb_view1.sigma_c_max
-    x1 = np.hstack((-Ll, ccb_view1.x_arr, Lr))
-    mu_epsf1 = np.hstack((ccb_view1.mu_epsf_arr[0], ccb_view1.mu_epsf_arr, ccb_view1.mu_epsf_arr[-1]))
-    epsm1 = np.hstack((ccb_view1.epsm_arr[0], ccb_view1.epsm_arr, ccb_view1.epsm_arr[-1]))
-    ax.plot(x1, mu_epsf1, lw=2, color='black')
-    ax.plot(x1, epsm1, lw=2, color='black')
-    
-    plt.xlabel('z [mm]')
-    plt.xlim(-35, 35)
-    plt.ylim(0)
-    plt.ylabel('$\epsilon$ [-]')
-
 def Vf_k(k):
     Em = ccb_view.model.E_m
     Ef = ccb_view.model.reinforcement_lst[0].E_f
     return -Em*k/(Ef*k-Em*k-Ef)
 
 def sigma_c_arr(k):
-    Vf = Vf_k(k[0])
-    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
-    sigma = ccb_view.sigma_c_arr(w_arr)
-    plt.plot(w_arr, sigma / Vf, lw=2)
-    Vf = Vf_k(k[1])
-    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
-    sigma = ccb_view.sigma_c_arr(w_arr)
-    plt.plot(w_arr, sigma / Vf, lw=2)
-    Vf = Vf_k(k[2])
-    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
-    ccb_view.model.reinforcement_lst = [reinf]
-    sigma = ccb_view.sigma_c_arr(w_arr)
-    plt.plot(w_arr, sigma / Vf, lw=2)
+    for ki in k:
+        Vf = Vf_k(ki)
+        reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
+        ccb_view.model.reinforcement_lst = [reinf]
+        sigma = ccb_view.sigma_c_arr(w_arr)
+        plt.plot(w_arr, sigma / Vf, lw=2)
+
 
 def profiles(k):
-    Vf = Vf_k(k[0])
-    ccb_view.model.w = 0.5
-    reinf = Reinforcement(r=0.01, tau=RV('uniform', loc=.1, scale=.002), V_f=Vf, E_f=200e3, xi=1000., n_int=50)
-    x = ccb_view.x_arr[1:-1]
-    epsm = ccb_view.epsm_arr[1:-1]
-    epsf = ccb_view.mu_epsf_arr[1:-1]
-    plt.plot(x, epsm, lw=2)
-    plt.plot(x, epsf, lw=2)
-    Vf = Vf_k(k[1])
-    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
-    ccb_view.model.reinforcement_lst = [reinf]
-    ccb_view.model.w = 0.5
-    x = ccb_view.x_arr[1:-1]
-    epsm = ccb_view.epsm_arr[1:-1]
-    epsf = ccb_view.mu_epsf_arr[1:-1]
-    plt.plot(x, epsm, lw=2)
-    plt.plot(x, epsf, lw=2)
-    Vf = Vf_k(k[2])
-    reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
-    ccb_view.model.reinforcement_lst = [reinf]
-    x = ccb_view.x_arr[1:-1]
-    epsm = ccb_view.epsm_arr[1:-1]
-    epsf = ccb_view.mu_epsf_arr[1:-1]
-    plt.plot(x, epsm, lw=2)
-    plt.plot(x, epsf, lw=2)
+    for ki in k:
+        Vf = Vf_k(ki)
+        ccb_view.model.w = 0.5
+        reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
+        ccb_view.model.reinforcement_lst = [reinf]
+        x = ccb_view.x_arr[1:-1]
+        epsm = ccb_view.epsm_arr[1:-1]
+        epsf = ccb_view.mu_epsf_arr[1:-1]
+        plt.plot(x, epsm, lw=2)
+        plt.plot(x, epsf, lw=2)
+
+
+def k_influence(k_arr):
+    epsf_max_lst = []
+    for ki in k_arr:
+        Vf = Vf_k(ki)
+        ccb_view.model.w = 0.5
+        reinf = Reinforcement(r=0.01, tau=0.1, V_f=Vf, E_f=200e3, xi=1000., n_int=50)
+        ccb_view.model.reinforcement_lst = [reinf]
+        epsf_max_lst.append(np.max(ccb_view.mu_epsf_arr))
+    plt.plot(k_arr, np.array(epsf_max_lst) * reinf.E_f)
+    plt.ylim(0)
+
+def sigma_c_arr_rand(k):
+    for ki in k:
+        Vf = Vf_k(ki)
+        reinf = Reinforcement(r=RV('uniform', loc=.005, scale=.01),
+                              tau=RV('uniform', loc=.05, scale=.1),
+                              V_f=Vf, E_f=200e3, xi=WeibullFibers(shape=5., sV0=0.003),
+                              n_int=50)
+        reinf = Reinforcement(r=0.01,#RV('uniform', loc=.005, scale=.01),
+                      tau=0.1,#RV('uniform', loc=.001, scale=.2),
+                      V_f=Vf, E_f=200e3, xi=WeibullFibers(shape=5., sV0=0.003),
+                      n_int=50)
+        ccb_view.model.reinforcement_lst = [reinf]
+        sigma = ccb_view.sigma_c_arr(w_arr)
+        plt.plot(w_arr, sigma / Vf, lw=2)
+
+
+def profiles_rand(k):
+    for ki in k:
+        Vf = Vf_k(ki)
+        ccb_view.model.w = 0.5
+        reinf = Reinforcement(r=RV('uniform', loc=.005, scale=.01),
+                      tau=RV('uniform', loc=.001, scale=.2),
+                      V_f=Vf, E_f=200e3, xi=WeibullFibers(shape=5., sV0=0.003),
+                      n_int=50)
+        ccb_view.model.reinforcement_lst = [reinf]
+        x = ccb_view.x_arr[1:-1]
+        epsm = ccb_view.epsm_arr[1:-1]
+        epsf = ccb_view.mu_epsf_arr[1:-1]
+        plt.plot(x, epsm, lw=2)
+        plt.plot(x, epsf, lw=2)
+
+
+def k_influence_rand(k_arr):
+    sig_max_lst = []
+    wmax_lst = []
+    for ki in k_arr:
+        print ki
+        Vf = Vf_k(ki)
+        reinf = Reinforcement(r=RV('uniform', loc=.005, scale=.01),
+                              tau=RV('uniform', loc=.05, scale=.1),
+                              xi=WeibullFibers(shape=5., sV0=0.003),
+                              V_f=Vf, E_f=200e3, n_int=100)
+        ccb_view.model.reinforcement_lst = [reinf]
+        sig_max, wmax = ccb_view.sigma_c_max
+        sig_max_lst.append(sig_max / Vf)
+        wmax_lst.append(wmax)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
+    ax1.plot(k_arr, np.array(sig_max_lst))
+    ax2.plot(k_arr, np.array(wmax_lst))
+    ax1.set_ylim(0)
+    ax2.set_ylim(0)
 
 #sigma_c_arr([0.1,0.3,0.5])
-profiles([0.1,0.3,0.5])
-#ld_rigid_vs_el_mtrx()
-#profiles_rigid_vs_el_mtrx()
-#elastic_matrix(np.linspace(.0, .5, 100))
-#sigma_f(np.linspace(.0, .3, 100))
-#rigid_mtrx()
+#profiles([0.1, 0.3, 0.5])
+#k_influence(np.linspace(0.01, 0.9, 10))
+#sigma_c_arr_rand([0.01, 0.5, 0.8])
+#profiles_rand([0.1, 0.3, 0.5])
+k_influence_rand(np.linspace(0.01, 0.9, 50))
 plt.legend(loc='best')
 plt.show()
