4722ca64d15372e2ba9e873649e170099624faa5
rostar <rrypl@.imb.rwth-aachen.de>
2013-08-05 17:33:05 +0200

    mult cracking added to CB view

--- scratch/CB/independent_fibers/CB_view.py
+++ scratch/CB/independent_fibers/CB_view.py
@@ -4,23 +4,16 @@ Created on 03.07.2013
 @author: acki
 '''
 import numpy as np
-from scipy.interpolate import interp1d 
-from scipy.optimize import minimize, basinhopping, brute
+from scipy.interpolate import interp1d
 import os
-from etsproxy.traits.api import HasTraits, Property, Callable, Array, cached_property
-from etsproxy.pyface.image_resource import ImageResource
-from etsproxy.traits.api import HasTraits, Float, Int, Event, Array, Interface, \
-    Tuple, Property, cached_property, Instance, Enum, on_trait_change, Dict
+from etsproxy.traits.api import HasTraits, Property, Array, cached_property, Float, Int, Instance, Event
 from etsproxy.traits.ui.api import Item, View, Group, HSplit, VGroup, Tabbed
 from etsproxy.traits.ui.menu import OKButton, CancelButton
-from math import sqrt
 from matplotlib.figure import Figure
 from quaducom.micro.resp_func.CB_rigid_mtrx_rand_xi import CBResidualRandXi
+from quaducom.micro.resp_func.CB_clamped_rigid_mtrx_rand_xi import CBFinResidualRandXi
 from spirrid.spirrid import SPIRRID
 from spirrid.rv import RV
-from matplotlib import pyplot as plt
-from scipy.special import gamma, gammainc
-from math import pi
 from util.traits.editors.mpl_figure_editor import MPLFigureEditor
 from etsproxy.traits.ui.api import ModelView
 
@@ -38,13 +31,22 @@ class Model(HasTraits):
     w_min = Float(auto_set=False, enter_set=True, params=True)
     w_max = Float(auto_set=False, enter_set=True, params=True)
     w_pts = Int(auto_set=False, enter_set=True, params=True)
+    w2_min = Float(auto_set=False, enter_set=True, params=True)
+    w2_max = Float(auto_set=False, enter_set=True, params=True)
+    w2_pts = Int(auto_set=False, enter_set=True, params=True)
     tau_scale = Float(auto_set=False, enter_set=True, params=True)
     tau_loc = Float(auto_set=False, enter_set=True, params=True)
     tau_shape = Float(auto_set=False, enter_set=True, params=True)
+    Ef = Float(auto_set=False, enter_set=True, params=True)
+    lm = Float(auto_set=False, enter_set=True, params=True)
 
     w = Property(Array)
     def _get_w(self):
         return np.linspace(self.w_min, self.w_max, self.w_pts)
+
+    w2 = Property(Array)
+    def _get_w2(self):
+        return np.linspace(self.w2_min, self.w2_max, self.w2_pts)
 
     interpolate_experiment = Property(depends_on='test_xdata, test_ydata')
     @cached_property
@@ -64,15 +66,38 @@ class Model(HasTraits):
         spirrid = SPIRRID(q=cb, sampling_type='PGrid')
         sV0 = self.sV0
         tau_scale = self.tau_scale
-        E_f = 180e3
         V_f = 1.0
-        r = 3.45e-3
+        r = 3.5e-3
         m = self.m
         tau = RV('weibull_min', shape=self.tau_shape, scale=tau_scale, loc=self.tau_loc)
         n_int = 100
         w = self.w
         spirrid.eps_vars=dict(w=w)
-        spirrid.theta_vars=dict(tau=tau, E_f=E_f, V_f=V_f, r=r, m=m, sV0=sV0)
+        spirrid.theta_vars=dict(tau=tau, E_f=self.Ef, V_f=V_f, r=r, m=m, sV0=sV0)
+        spirrid.n_int=n_int
+        if isinstance(r, RV):
+            r_arr = np.linspace(r.ppf(0.001), r.ppf(0.999), 300)
+            Er = np.trapz(r_arr ** 2 * r.pdf(r_arr), r_arr)
+        else:
+            Er = r ** 2
+        sigma_c = spirrid.mu_q_arr / Er
+        return sigma_c
+
+    model_extrapolate = Property(Array)
+    def _get_model_extrapolate(self):
+        cb = CBFinResidualRandXi()
+        spirrid = SPIRRID(q=cb, sampling_type='PGrid')
+        sV0 = self.sV0
+        tau_scale = self.tau_scale
+        V_f = 1.0
+        r = 3.5e-3
+        m = self.m
+        tau = RV('weibull_min', shape=self.tau_shape, scale=tau_scale, loc=self.tau_loc)
+        n_int = 100
+        w = self.w2
+        lm = self.lm
+        spirrid.eps_vars=dict(w=w)
+        spirrid.theta_vars=dict(tau=tau, E_f=self.Ef, V_f=V_f, r=r, m=m, sV0=sV0, lm=lm)
         spirrid.n_int=n_int
         if isinstance(r, RV):
             r_arr = np.linspace(r.ppf(0.001), r.ppf(0.999), 300)
@@ -95,10 +120,15 @@ class CBView(ModelView):
     def _figure_default(self):
         figure = Figure(facecolor='white')
         return figure
+    
+    figure2 = Instance(Figure)
+    def _figure2_default(self):
+        figure = Figure(facecolor='white')
+        return figure
 
     data_changed = Event
 
-    def plot(self, fig):
+    def plot(self, fig, fig2):
         figure = fig
         figure.clear()
         axes = figure.gca()
@@ -110,9 +140,17 @@ class CBView(ModelView):
         axes.plot(self.model.w, self.model.interpolate_experiment2(self.model.w), lw=1.0, color='black', \
                   label='experiment2')
         axes.legend()
+        
+        figure2 = fig2
+        figure2.clear()
+        axes = figure2.gca()
+        # plot PDF
+        axes.plot(self.model.w2, self.model.model_extrapolate, lw=2.0, color='red', \
+                  label='model')
+        axes.legend()
 
     def refresh(self):
-        self.plot(self.figure)
+        self.plot(self.figure, self.figure2)
         self.data_changed = True
 
     traits_view = View(HSplit(VGroup(Group(Item('model.tau_scale'),
@@ -120,9 +158,14 @@ class CBView(ModelView):
                                            Item('model.tau_loc'),
                                            Item('model.m'),
                                            Item('model.sV0'),
+                                           Item('model.Ef'),
                                            Item('model.w_min'),
                                            Item('model.w_max'),
                                            Item('model.w_pts'),
+                                           Item('model.w2_min'),
+                                           Item('model.w2_max'),
+                                           Item('model.w2_pts'),
+                                           Item('model.lm'),
                                            ),
                                       id='pdistrib.distr_type.pltctrls',
                                       label='Distribution parameters',
@@ -137,6 +180,17 @@ class CBView(ModelView):
                                             ),
                                         label='Plot',
                                         id='pdistrib.figure.params',
+                                        dock='tab',
+                                       ),
+                              Tabbed(Group(Item('figure2',
+                                            editor=MPLFigureEditor(),
+                                            show_label=False,
+                                            resizable=True),
+                                            scrollable=True,
+                                            label='Plot',
+                                            ),
+                                        label='Plot',
+                                        id='pdistrib.figure2',
                                         dock='tab',
                                        ),
                                 dock='tab',
@@ -159,9 +213,11 @@ if __name__ == '__main__':
     from scipy.special import gamma, gammainc
     from math import pi
 
-    model = Model(w_min=0.0, w_max=8.0, w_pts=500,
-                  sV0=3.2e-3, m=5.0, tau_scale=0.04,
-                  tau_shape=0.22, tau_loc=0.007)
+    model = Model(w_min=0.0, w_max=8.0, w_pts=200,
+                  w2_min=0.0, w2_max=.5, w2_pts=200,
+                  sV0=2.6e-3, m=5.0, tau_scale=0.03,
+                  tau_shape=0.23, tau_loc=0.006, Ef=240e3,
+                  lm=20.)
 
     file1 = open('DATA/PO01_RYP.ASC', 'r')
     model.test_xdata = - np.loadtxt(file1, delimiter=';')[:,3]
