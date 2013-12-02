50f00265720983464acd1677a7c2becb297abaf2
rostar <rrypl@.imb.rwth-aachen.de>
2013-12-02 22:02:48 +0100

    disss figs

--- scratch/diss_figs/interacting.py
+++ scratch/diss_figs/interacting.py
@@ -28,9 +28,9 @@ reinf = ContinuousFibers(r=0.0035,
                       tau=0.1,
                       V_f=0.05,
                       E_f=180e3,
-                      xi=fibers_MC(m=4.0, sV0=0.003),
+                      xi=fibers_MC(m=5.0, sV0=0.003),
                       label='carbon',
-                      n_int=10)
+                      n_int=200)
 
 model = CompositeCrackBridge(E_m=25e3,
                              reinforcement_lst=[reinf],
@@ -41,9 +41,8 @@ model = CompositeCrackBridge(E_m=25e3,
 ccb_view = CompositeCrackBridgeView(model=model)
 
 def BC_effect(w_arr):
-    reinf.xi = 100.
-    model.Ll = 30.
-    model.Lr = 50.
+    model.Ll = 20.
+    model.Lr = 30.
     sigma_c_arr = ccb_view.sigma_c_arr(w_arr)
     plt.plot(w_arr, sigma_c_arr, lw=2, color='black', label='w-sigma')
 
@@ -51,7 +50,7 @@ def BC_effect(w_arr):
 # energy(np.linspace(.0, .15, 100))
 #    sigma_c = np.linspace(1., 7., 7)
 # profile(0.031)
-w = np.linspace(0.0, 1.6, 200)
+w = np.linspace(0.0, .6, 200)
 BC_effect(w)
 # energy(w)
 # bundle at 20 mm
