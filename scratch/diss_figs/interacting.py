174b18fe78524218f2192dcf913ddb5a9e08edb6
rostar <rrypl@.imb.rwth-aachen.de>
2013-12-04 22:40:27 +0100

    diss figs dependent fibers

--- scratch/diss_figs/interacting.py
+++ scratch/diss_figs/interacting.py
@@ -17,18 +17,18 @@ from spirrid.spirrid import SPIRRID
 from spirrid.rv import RV
 from matplotlib import pyplot as plt
 from scipy.stats import weibull_min
-from stats.pdistrib.weibull_fibers_composite_distr import fibers_MC, fibers_CB_rigid, fibers_dry
+from stats.pdistrib.weibull_fibers_composite_distr import fibers_MC, fibers_CB_rigid, fibers_dry, fibers_CB_elast
 from math import pi
 from quaducom.meso.homogenized_crack_bridge.elastic_matrix.reinforcement import ContinuousFibers
 from stats.pdistrib.weibull_fibers_composite_distr import WeibullFibers, fibers_MC
 from quaducom.meso.homogenized_crack_bridge.elastic_matrix.hom_CB_elastic_mtrx_view import CompositeCrackBridge, CompositeCrackBridgeView
-
+import copy
 
 reinf = ContinuousFibers(r=0.0035,
                       tau=0.1,
                       V_f=0.05,
-                      E_f=180e3,
-                      xi=fibers_MC(m=5.0, sV0=0.003),
+                      E_f=200e3,
+                      xi=fibers_MC(m=5.0, sV0=0.006),
                       label='carbon',
                       n_int=200)
 
@@ -46,17 +46,39 @@ def BC_effect(w_arr):
     sigma_c_arr = ccb_view.sigma_c_arr(w_arr)
     plt.plot(w_arr, sigma_c_arr, lw=2, color='black', label='w-sigma')
 
-# TODO: check energy for combined reinf
-# energy(np.linspace(.0, .15, 100))
-#    sigma_c = np.linspace(1., 7., 7)
-# profile(0.031)
-w = np.linspace(0.0, .6, 200)
-BC_effect(w)
-# energy(w)
-# bundle at 20 mm
-# sigma_bundle = 70e3*w/20.*np.exp(-(w/20./0.03)**5.)
-# plt.plot(w,sigma_bundle)
-# plt.plot(ccb_view.sigma_c_max[1], ccb_view.sigma_c_max[0], 'bo')
-# sigma_f(np.linspace(.0, .16, 50))
+def FBM_limit(w_arr):
+    sV0 = 0.006
+    m = 5.0
+    model.Ll = 50.
+    model.Lr = 50.
+    model.E_m = 25e3
+    reinf.tau = 0.2
+    reinf.xi = fibers_CB_elast(m=m, sV0=sV0)
+    sigma_c_arr = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr / 100., sigma_c_arr / 0.05, lw=2, label='w-sigma1')
+    reinf1 = copy.copy(reinf)
+    reinf1.tau = 0.1
+    model.reinforcement_lst = [reinf1]
+    sigma_c_arr = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr / 100., sigma_c_arr / 0.05, lw=2, label='w-sigma2')
+    reinf2 = copy.copy(reinf)
+    reinf2.tau = 0.05
+    model.reinforcement_lst = [reinf2]
+    sigma_c_arr = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr / 100., sigma_c_arr / 0.05, lw=2, label='w-sigma2')
+    reinf3 = copy.copy(reinf)
+    reinf3.tau = 0.005
+    model.reinforcement_lst = [reinf3]
+    sigma_c_arr = ccb_view.sigma_c_arr(w_arr)
+    plt.plot(w_arr / 100., sigma_c_arr / 0.05, lw=2, label='w-sigma2')
+
+    e = w_arr / (100.)
+    F = lambda x: 1 - np.exp(-100. * reinf.r ** 2 * pi * (x / sV0) ** m)
+    sigma = e * 200e3 * (1. - F(e))
+    plt.plot(e, sigma, lw=4, color='red', label='FBM')
+
+# BC_effect(np.linspace(0.0, .6, 200))
+FBM_limit(np.linspace(0.0, 3.0, 200))
+
 plt.legend(loc='best')
 plt.show()
