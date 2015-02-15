7eeba83a8325d7ca99a6e01d2bf9f7e9938658bf
admin <admin@M4700.stm.fce.vutbr.cz>
2015-02-15 06:54:20 +0100

    new modules added

---
+++ scratch/diss_figs/calibration.py
@@ -0,0 +1,177 @@
+import numpy as np
+from scipy.interpolate import interp1d
+import os
+from etsproxy.traits.api import HasTraits, Property, Array, \
+     cached_property, Float, Int
+from quaducom.micro.resp_func.CB_clamped_rand_xi import CBClampedRandXi
+from spirrid.spirrid import SPIRRID
+from spirrid.rv import RV
+from etsproxy.util.home_directory import get_home_directory
+from scipy.optimize import minimize
+from matplotlib import pyplot as plt
+
+
+class Model(HasTraits):
+
+    test_xdata = Array
+    test_ydata = Array
+    sV0 = Float(0.0069, auto_set=False, enter_set=True, params=True)
+    m = Float(7.1, auto_set=False, enter_set=True, params=True)
+    w_min = Float(0.0, auto_set=False, enter_set=True, params=True)
+    w_max = Float(2., auto_set=False, enter_set=True, params=True)
+    w_pts = Int(100, auto_set=False, enter_set=True, params=True)
+    n_int = Int(100, auto_set=False, enter_set=True, params=True)
+    tau_scale = Float(1.0, auto_set=False, enter_set=True, params=True)
+    tau_loc = Float(0.005, auto_set=False, enter_set=True, params=True)
+    tau_shape = Float(0.2, auto_set=False, enter_set=True, params=True)
+    Ef = Float(181e3, auto_set=False, enter_set=True, params=True)
+    lm = Float(13.0, auto_set=False, enter_set=True, params=True)
+    V_f = Float(1.0, params=True)
+    r = Float(3.5e-3, params=True)
+
+    w = Property(Array)
+    def _get_w(self):
+        return np.linspace(self.w_min, self.w_max, self.w_pts)
+
+    interpolate_experiment = Property(depends_on='test_xdata, test_ydata')
+    @cached_property
+    def _get_interpolate_experiment(self):
+        interp = interp1d(self.test_xdata, self.test_ydata,
+                        bounds_error=False, fill_value=0.0)
+        return interp(self.w)
+
+    def model_free(self, tau_shape, tau_scale, tau_loc):
+        cb = CBClampedRandXi(pullout=False)
+        spirrid = SPIRRID(q=cb, sampling_type='LHS')
+        sV0 = self.sV0
+        V_f = self.V_f
+        r = self.r
+        m = self.m
+        tau = RV('gamma', shape=tau_shape, scale=tau_scale, loc=tau_loc)
+        n_int = self.n_int
+        w = self.w
+        lm = 1000.
+        spirrid.eps_vars = dict(w=w)
+        spirrid.theta_vars = dict(tau=tau, E_f=self.Ef, V_f=V_f, r=r, m=m, sV0=sV0, lm=lm)
+        spirrid.n_int = n_int * 10
+        sigma_c = spirrid.mu_q_arr / self.r ** 2
+        return sigma_c
+
+    def model_clamped(self, tau_shape, tau_scale, tau_loc):
+        cb = CBClampedRandXi(pullout=False)
+        spirrid = SPIRRID(q=cb, sampling_type='LHS')
+        sV0 = self.sV0
+        V_f = self.V_f
+        r = self.r
+        m = self.m
+        tau = RV('gamma', shape=tau_shape, scale=tau_scale, loc=tau_loc)
+        n_int = self.n_int
+        w = self.w2
+        lm = self.lm
+        spirrid.eps_vars = dict(w=w)
+        spirrid.theta_vars = dict(tau=tau, E_f=self.Ef, V_f=V_f, r=r, m=m, sV0=sV0, lm=lm)
+        spirrid.n_int = n_int
+        sigma_c = spirrid.mu_q_arr / self.r ** 2
+        return sigma_c
+   
+    lack = 1e10
+   
+    def lack_of_fit(self, params):
+        tau_shape = params[0]
+        tau_scale = params[1]
+        tau_loc = 0.0#params[2]
+        weights = np.linspace(10.0, 0.0, len(self.w))
+        lack = np.sum(weights * (self.model_free(tau_shape, tau_scale, tau_loc) - self.interpolate_experiment) ** 2)
+        print 'params = ', params
+        print 'lack of fit', lack
+        if lack < self.lack:
+            self.lack = lack
+            plt.ion()
+            plt.cla()
+            plt.plot(self.w, self.interpolate_experiment, color='black')
+            plt.plot(self.w, self.model_free(tau_shape, tau_scale, tau_loc), color='red', lw=2)
+            plt.draw()
+        return lack
+
+    def eval_params(self):
+        # params for CB1 = 0.090, 0.901, 0.0
+        # params for CB2 = 0.130, 0.255, 0.0
+        # params for CB3 = 0.064, 2.185, 0.0
+        # params for CB4 = 0.096, 0.409, 0.0
+        # params for CB5 = 0.109, 0.795, 0.0
+        params = minimize(self.lack_of_fit, np.array([0.109, 0.795]),
+                          method='L-BFGS-B', bounds=((0.05, 1.), (0.01, 3.)))
+        return params
+
+if __name__ == '__main__':
+
+    model = Model()
+
+    home_dir = get_home_directory()
+    path = [home_dir, 'git',  # the path of the data file
+            'rostar',
+            'scratch',
+            'diss_figs',
+            'CB5.txt']
+    filepath = os.path.join(*path)
+    file1 = open(filepath, 'r')
+    cb = np.loadtxt(file1, delimiter=';')
+    model.test_xdata = -cb[:, 2] / 4. - cb[:, 3] / 4. - cb[:, 4] / 2.
+    model.test_ydata = cb[:, 1] / (11. * 0.445) * 1000
+
+    #params = model.eval_params().x
+    #print 'parameters: ', params
+    #shape, scale = params
+    #loc = 0.0
+    #plt.plot(model.w, model.interpolate_experiment, color='black', lw=1, label='experiment')
+    #plt.plot(model.w, model.model_free(shape, scale, loc), color='blue', lw=2, label='model')
+    
+    CB1 = 0.090, 0.901, 0.0
+    CB2 = 0.130, 0.255, 0.0
+    CB3 = 0.064, 2.185, 0.0
+    CB4 = 0.096, 0.409, 0.0
+    CB5 = 0.109, 0.795, 0.0
+    CBs = [CB1, CB2, CB3, CB4, CB5]
+    
+    for i in range(6):
+        if i == 5:
+            shape, scale, loc = np.mean(np.array(CBs), axis=0)
+            print shape, scale, loc
+            plt.figure()
+            for i in range(5):
+                path = [home_dir, 'git',  # the path of the data file
+                'rostar',
+                'scratch',
+                'diss_figs',
+                'CB' + str(i+1) +'.txt']
+                filepath = os.path.join(*path)
+                file1 = open(filepath, 'r')
+                cb = np.loadtxt(file1, delimiter=';')
+                model.test_xdata = -cb[:, 2] / 4. - cb[:, 3] / 4. - cb[:, 4] / 2.
+                model.test_ydata = cb[:, 1] / (11. * 0.445) * 1000
+                plt.plot(model.w, model.interpolate_experiment, color='black', lw=1, label='experiment ' + str(i+1))
+            plt.plot(model.w, model.model_free(shape, scale, loc), color='blue', lw=2, label='model')
+            plt.legend(loc='best')
+            plt.xlabel('crack opening [mm]')
+            plt.ylabel('fiber stress [MPa]')
+        else:
+            shape, scale, loc = CBs[i]
+            plt.figure()
+            path = [home_dir, 'git',  # the path of the data file
+                'rostar',
+                'scratch',
+                'diss_figs',
+                'CB' + str(i+1) +'.txt']
+            filepath = os.path.join(*path)
+            file1 = open(filepath, 'r')
+            cb = np.loadtxt(file1, delimiter=';')
+            model.test_xdata = -cb[:, 2] / 4. - cb[:, 3] / 4. - cb[:, 4] / 2.
+            model.test_ydata = cb[:, 1] / (11. * 0.445) * 1000
+            plt.plot(model.w, model.interpolate_experiment, color='black', lw=1, label='experiment ' + str(i+1))
+            plt.plot(model.w, model.model_free(shape, scale, loc), color='blue', lw=2, label='model')
+            plt.legend(loc='best')
+            plt.xlabel('crack opening [mm]')
+            plt.ylabel('fiber stress [MPa]')
+    plt.show()
+    
+
---
+++ scratch/diss_figs/validation_rostar.py
@@ -0,0 +1,206 @@
+'''
+Created on 16 Feb 2014
+
+@author: Q
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+from quaducom.meso.homogenized_crack_bridge.rigid_matrix.CB_view import CBView, Model
+from spirrid.rv import RV
+from stats.misc.random_field.random_field_1D import RandomField
+from quaducom.meso.scm.numerical.interdependent_fibers.scm_interdependent_fibers_model import SCM
+from quaducom.meso.scm.numerical.interdependent_fibers.scm_interdependent_fibers_view import SCMView
+from quaducom.meso.homogenized_crack_bridge.elastic_matrix.hom_CB_elastic_mtrx import CompositeCrackBridge
+from quaducom.micro.resp_func.CB_clamped_rand_xi import CBClampedRandXi
+from spirrid.spirrid import SPIRRID
+from scipy.optimize import minimize_scalar
+
+def CB():
+    model = Model(w_min=0.0, w_max=8.0, w_pts=100,
+                  w2_min=0.0, w2_max=.3, w2_pts=50,
+                  sV0=0.0094, m=9.0, tau_loc=0.00, Ef=180e3,
+                  lm=15., n_int=100)
+    
+    for i in range(5):
+        cb = np.loadtxt("CB" + str(i+1) + ".txt", delimiter=';')
+        model.test_xdata.append(-cb[:,2]/4. - cb[:,3]/4. - cb[:,4]/2.)
+        model.test_ydata.append(cb[:,1] / (11. * 0.445) * 1000)
+    
+    cb = CBView(model=model)
+    cb.refresh()
+    cb.configure_traits()
+    
+def TT():
+    for i in range(5):
+        file_i = np.loadtxt("TT-4C-0" + str(i+1) + ".txt", delimiter=';')
+        plt.plot(-file_i[:,2]/2./250. - file_i[:,3]/2./250.,file_i[:,1]/2., lw=1, color='blue')
+        file_i2 = np.loadtxt("TT-6C-0" + str(i+1) + ".txt", delimiter=';')
+        plt.plot(-file_i2[:,2]/2./250. - file_i2[:,3]/2./250.,file_i2[:,1]/2., lw=1, color='red')
+    plt.legend(loc='best')
+
+def valid():
+    TT()
+    from quaducom.meso.homogenized_crack_bridge.elastic_matrix.reinforcement import ContinuousFibers
+    from stats.pdistrib.weibull_fibers_composite_distr import WeibullFibers, fibers_MC
+    length = 300.
+    nx = 3000
+    tau_scale =1.03222648341
+    tau_shape = 0.0782374375177
+    tau_loc = 0.00
+    xi_shape = 9.0
+    xi_scale = 0.009
+    ld = True
+    w_width = False
+    w_density = False
+    
+    random_field1 = RandomField(seed=False,
+                               lacor=1.,
+                               length=length,
+                               nx=900,
+                               nsim=1,
+                               loc=.0,
+                               shape=50.,
+                               scale=1.3*3.4,
+                               distr_type='Weibull'
+                               )
+ 
+    reinf1 = ContinuousFibers(r=3.5e-3,
+                              tau=RV('gamma', loc=tau_loc, scale=tau_scale, shape=tau_shape),
+                              V_f=0.015,
+                              E_f=200e3,
+                              xi=fibers_MC(m=xi_shape, sV0=xi_scale),
+                              label='carbon',
+                              n_int=500)
+ 
+    CB_model1 = CompositeCrackBridge(E_m=25e3,
+                                 reinforcement_lst=[reinf1],
+                                 )
+ 
+    scm1 = SCM(length=length,
+              nx=nx,
+              random_field=random_field1,
+              CB_model=CB_model1,
+              load_sigma_c_arr=np.linspace(0.01, 32., 100),
+              n_BC_CB=12
+              )
+ 
+    scm_view1 = SCMView(model=scm1)
+    scm_view1.model.evaluate()
+    eps1, sigma1 = scm_view1.eps_sigma
+    
+#     random_field2 = RandomField(seed=False,
+#                                lacor=1.,
+#                                length=length,
+#                                nx=700,
+#                                nsim=1,
+#                                loc=.0,
+#                                shape=80.,
+#                                scale=1.3 * 3.,
+#                                distr_type='Weibull'
+#                                )
+#       
+#     reinf2 = ContinuousFibers(r=3.5e-3,
+#                               tau=RV('gamma', loc=tau_loc, scale=tau_scale, shape=tau_shape),
+#                               V_f=0.015,
+#                               E_f=200e3,
+#                               xi=fibers_MC(m=xi_shape, sV0=xi_scale),
+#                               label='carbon',
+#                               n_int=500)
+#       
+#     CB_model2 = CompositeCrackBridge(E_m=25e3,
+#                                  reinforcement_lst=[reinf2],
+#                                  )
+#       
+#     scm2 = SCM(length=length,
+#               nx=nx,
+#               random_field=random_field2,
+#               CB_model=CB_model2,
+#               load_sigma_c_arr=np.linspace(0.01, 27., 100),
+#               n_BC_CB=15
+#               )
+#       
+#     scm_view2 = SCMView(model=scm2)
+#     scm_view2.model.evaluate()
+#     eps2, sigma2 = scm_view2.eps_sigma
+
+    if ld == True:
+        plt.plot(eps1, sigma1, color='black', lw=2,
+                 label='cracks1.0: ' + str(len(scm1.cracks_list[-1]))
+                 + ' scale ' + str(tau_scale)
+                 + ' shape ' + str(tau_shape)
+                 + ' loc ' + str(tau_loc))
+#        plt.plot(eps2, sigma2, color='black', lw=2, label='cracks: ' + str(len(scm2.cracks_list[-1])))    
+        plt.xlabel('composite strain [-]')
+        plt.ylabel('composite stress [MPa]')
+        plt.legend(loc='best')
+
+    if w_width == True:
+        plt.figure()
+        plt.plot(scm_view1.model.load_sigma_c_arr, scm_view1.w_mean)
+        plt.plot(scm_view1.model.load_sigma_c_arr, scm_view1.w_max)
+#         plt.plot(scm_view2.model.load_sigma_c_arr, scm_view2.w_mean)
+#         plt.plot(scm_view2.model.load_sigma_c_arr, scm_view2.w_max)
+         
+    if w_density == True:
+        plt.figure()
+        plt.plot(scm_view1.model.load_sigma_c_arr, scm_view1.w_density)
+#         plt.plot(scm_view2.model.load_sigma_c_arr, scm_view2.w_density)   
+
+
+
+
+def simplified():
+    cb = CBClampedRandXi()
+    spirrid = SPIRRID(q=cb, sampling_type='PGrid',
+                  theta_vars=dict(tau=RV('gamma', loc=0.0055, scale=0.7, shape=0.2),
+                                  E_f=200e3,
+                                  V_f=0.01,
+                                  r=0.00345,
+                                  m=7.0,
+                                  sV0=0.0042,
+                                  lm=1000.),
+                        n_int=200)
+    def sigmac(w, lm):
+        spirrid.eps_vars['w'] = np.array([w])
+        spirrid.theta_vars['lm'] = lm
+        sigma_c = spirrid.mu_q_arr / spirrid.theta_vars['r'] ** 2
+        return sigma_c
+
+    def maxsigma(lm):
+        def minfunc(w):
+            res = sigmac(w, lm)
+            return -res * (w < 200.) + 1e-5 * w ** 2
+        w_max = minimize_scalar(minfunc, bracket=(0.0001, 0.0002))
+        return w_max.x, sigmac(w_max.x, lm) / spirrid.theta_vars['V_f']
+
+    sigmaf = []
+    w_lst = []
+    lcs = 1./np.linspace(8.4, 300.0, 20)
+    for lcsi in lcs:
+        print lcsi
+        wi, sigi = maxsigma(1./lcsi)
+        sigmaf.append(sigi)
+        w_lst.append(wi)
+    plt.plot(lcs, sigmaf)
+    plt.plot(1./13.7, 1201, 'ro')
+    plt.plot(1./9.7, 1373, 'bo')
+    plt.errorbar(1./13.7, 1201, 104.4)
+    plt.errorbar(1./9.7, 1373, 36.4)
+    plt.ylim(0)
+    plt.figure()
+    plt.plot(lcs, w_lst)
+    plt.plot(1./13.7, 0.088, 'ro')
+    plt.plot(1./9.7, 0.05, 'bo')
+    plt.errorbar(1./13.7, 0.088, 0.003)
+    plt.errorbar(1./9.7, 0.05, 0.005)
+    plt.ylim(0)
+    plt.show()
+
+if __name__ == '__main__':
+    #simplified()
+    valid()
+    #CB()
+    #TT()
+    plt.show()
+    
